1053b031feb2e9a54fdb40425e605b29ef29ca00
Kelvin Fichter <=>
2018-08-14 15:51:58 +0200

    Use merkle tree with variable depth

--- contracts/ECRecovery.sol
+++ contracts/ECRecovery.sol
@@ -2,17 +2,22 @@ pragma solidity ^0.4.0;
 
 
 /**
- * @title Eliptic curve signature operations
- * @dev Based on https://gist.github.com/axic/5b33912c6f61ae6fd96d6c4a47afde6d
+ * @title ECRecovery
+ * @dev Elliptic curve signature operations.
+ * Based off of https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/ECRecovery.sol.
  */
 library ECRecovery {
+    /*
+     * Internal functions
+     */
 
-  /**
-   * @dev Recover signer address from a message by using his signature
-   * @param hash bytes32 message, the hash is the signed message. What is recovered is the signer address.
-   * @param sig bytes signature, the signature is generated using web3.eth.sign()
-   */
-    function recover(bytes32 hash, bytes sig)
+    /**
+     * @dev Given a hash and a signature, returns the address of the signee.
+     * @param _hash Hash that was signed.
+     * @param _sig Signature over the hash.
+     * @return Address of the signee.
+     */
+    function recover(bytes32 _hash, bytes _sig)
         internal
         pure
         returns (address)
@@ -21,28 +26,28 @@ library ECRecovery {
         bytes32 s;
         uint8 v;
 
-        //Check the signature length
-        if (sig.length != 65) {
-            return (address(0));
+        // Check the signature length.
+        if (_sig.length != 65) {
+            revert();
         }
 
-        // Divide the signature in v, r, and s variables
+        // Divide the signature in v, r, and s variables.
         assembly {
-            r := mload(add(sig, 32))
-            s := mload(add(sig, 64))
-            v := byte(0, mload(add(sig, 96)))
+            r := mload(add(_sig, 32))
+            s := mload(add(_sig, 64))
+            v := byte(0, mload(add(_sig, 96)))
         }
 
-        // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
+        // Version of signature should be 27 or 28, but 0 and 1 are also possible versions.
         if (v < 27) {
             v += 27;
         }
 
-        // If the version is correct return the signer address
+        // If the version is correct return the signer address.
         if (v != 27 && v != 28) {
-            return (address(0));
+            revert();
         } else {
-            return ecrecover(hash, v, r, s);
+            return ecrecover(_hash, v, r, s);
         }
     }
 }
--- plasma_core/utils/merkle/fixed_merkle.py
+++ plasma_core/utils/merkle/fixed_merkle.py
@@ -1,76 +1,91 @@
 from ethereum.utils import sha3
-from plasma_core.constants import NULL_HASH
 from .exceptions import MemberNotExistException
-from .node import Node
+from plasma_core.constants import NULL_HASH
+
+
+class MerkleNode(object):
+
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
 
 
 class FixedMerkle(object):
 
     def __init__(self, depth, leaves=[], hashed=False):
         if depth < 1:
-            raise ValueError('depth should be at least 1')
+            raise ValueError('depth must be at least 1')
 
         self.depth = depth
         self.leaf_count = 2 ** depth
         self.hashed = hashed
 
         if len(leaves) > self.leaf_count:
-            raise ValueError('num of leaves exceed max avaiable num with the depth')
+            raise ValueError('number of leaves should be at most depth ** 2')
 
         if not hashed:
             leaves = [sha3(leaf) for leaf in leaves]
-        self.leaves = leaves + [NULL_HASH] * (self.leaf_count - len(leaves))
-        self.tree = [self.create_nodes(self.leaves)]
-        self.create_tree(self.tree[0])
 
-    def create_nodes(self, leaves):
-        return [Node(leaf) for leaf in leaves]
+        self.leaves = leaves + [sha3(NULL_HASH)] * (self.leaf_count - len(leaves))
+        self.tree = [self.__create_nodes(self.leaves)]
+        self.__create_tree(self.tree[0])
 
-    def create_tree(self, leaves):
+    def __create_nodes(self, leaves):
+        return [MerkleNode(leaf) for leaf in leaves]
+
+    def __create_tree(self, leaves):
         if len(leaves) == 1:
             self.root = leaves[0].data
-            return self.root
+            return
+
         next_level = len(leaves)
         tree_level = []
+
         for i in range(0, next_level, 2):
             combined = sha3(leaves[i].data + leaves[i + 1].data)
-            next_node = Node(combined, leaves[i], leaves[i + 1])
+            next_node = MerkleNode(combined, leaves[i], leaves[i + 1])
             tree_level.append(next_node)
+
         self.tree.append(tree_level)
-        self.create_tree(tree_level)
+        self.__create_tree(tree_level)
 
     def check_membership(self, leaf, index, proof):
         if not self.hashed:
             leaf = sha3(leaf)
         computed_hash = leaf
+        computed_index = index
+
         for i in range(0, self.depth * 32, 32):
-            segment = proof[i:i + 32]
-            if index % 2 == 0:
-                computed_hash = sha3(computed_hash + segment)
+            proof_segment = proof[i:i + 32]
+
+            if computed_index % 2 == 0:
+                computed_hash = sha3(computed_hash + proof_segment)
             else:
-                computed_hash = sha3(segment + computed_hash)
-            index = index // 2
+                computed_hash = sha3(proof_segment + computed_hash)
+            computed_index = computed_index // 2
+
         return computed_hash == self.root
 
     def create_membership_proof(self, leaf):
         if not self.hashed:
             leaf = sha3(leaf)
-        if not self.is_member(leaf):
+        if not self.__is_member(leaf):
             raise MemberNotExistException('leaf is not in the merkle tree')
 
         index = self.leaves.index(leaf)
         proof = b''
+
         for i in range(0, self.depth, 1):
             if index % 2 == 0:
                 sibling_index = index + 1
             else:
                 sibling_index = index - 1
             index = index // 2
+
             proof += self.tree[i][sibling_index].data
+
         return proof
 
-    def is_member(self, leaf):
+    def __is_member(self, leaf):
         return leaf in self.leaves
-
-    def not_member(self, leaf):
-        return leaf not in self.leaves
