6ffb5bb137d3773510d801fec8566b82436a051b
David Knott <david1k1nott@gmail.com>
2018-11-07 17:38:51 +0700

    Being to change testlang to morevp format

--- testlang/testlang.py
+++ testlang/testlang.py
@@ -2,10 +2,13 @@ from plasma_core.child_chain import ChildChain
 from plasma_core.account import EthereumAccount
 from plasma_core.block import Block
 from plasma_core.transaction import Transaction
+from plasma_core.transaction_v2 import Transaction as TransactionV2
+from plasma_core.block_v2 import Block as BlockV2
 from plasma_core.constants import NULL_ADDRESS
 from plasma_core.utils.signatures import sign
 from plasma_core.utils.transactions import decode_utxo_id, encode_utxo_id
 from plasma_core.utils.address import address_to_hex
+from plasma_core.utils.merkle.fixed_merkle import FixedMerkle
 from ethereum.utils import sha3
 import conftest
 
@@ -26,6 +29,40 @@ def get_accounts(ethtester):
         key = getattr(ethtester, 'k{0}'.format(i))
         accounts.append(EthereumAccount(address_to_hex(address), key))
     return accounts
+
+
+class InFlightExit(object):
+    
+    def __init__(self, root_chain, in_flight_tx, exit_start_timestamp, exit_map, bond_owner, oldest_competitor):
+        self.root_chain = root_chain
+        self.in_flight_tx = in_flight_tx
+        self.exit_start_timestamp = exit_start_timestamp
+        self.exit_map = exit_map
+        self.bond_owner = bond_owner
+        self.oldest_competitor = oldest_competitor
+        self.inputs = {}
+        self.outputs = {}
+
+    @property
+    def challenge_flag_set(self):
+        return self.root_chain.flagSet(self.exit_start_timestamp)
+
+    def get_input(self, index):
+        input_info = self.inputs.get(index)
+        if not input_info:
+            input_info = TransactionOutput(*self.root_chain.getInFlightExitOutput(self.in_flight_tx.encoded, index))
+            input_info.owner = address_to_hex(input_info.owner)
+            self.inputs[index] = input_info
+        return input_info
+
+    def get_output(self, index):
+        return self.get_input(index + 4)
+
+    def input_piggybacked(self, index):
+        return (self.exit_map >> index & 1) == 1
+
+    def output_piggybacked(self, index):
+        return self.input_piggybacked(index + 4)
 
 
 class StandardExit(object):
@@ -74,7 +111,6 @@ class TestingLanguage(object):
         self.accounts = get_accounts(ethtester)
         self.operator = self.accounts[0]
         self.child_chain = ChildChain(self.accounts[0].address)
-        self.confirmations = {}
 
         # morevp semantic
         self.root_chain.blocks = self.root_chain.childChain
@@ -84,43 +120,60 @@ class TestingLanguage(object):
         """Current chain timestamp"""
         return self.ethtester.chain.head_state.timestamp
 
-    def deposit(self, owner, amount):
-        if isinstance(owner, str):
-            for acc in self.accounts:
-                if owner == acc.address:
-                    owner = acc
-                    break
-            if isinstance(owner, str):
-                raise ValueError("owner must be a known account")
-        blknum = self.deposit_pre_morevp(owner, amount)
-        return encode_utxo_id(blknum, 0, 0)
 
-    def deposit_pre_morevp(self, owner, amount):
-        """Creates a deposit transaction for a given owner and amount.
-
-        Args:
-            owner (EthereumAccount): Account to own the deposit.
-            amount (int): Deposit amount.
-
-        Returns:
-            int: Unique identifier of the deposit.
-        """
-
-        deposit_tx = Transaction(0, 0, 0,
-                                 0, 0, 0,
-                                 NULL_ADDRESS,
-                                 owner.address, amount,
-                                 NULL_ADDRESS, 0)
-
-        blknum = self.root_chain.getDepositBlock()
-        pre_balance = self.get_balance(self.root_chain)
-        self.root_chain.deposit(value=amount, sender=owner.key)
-        balance = self.get_balance(self.root_chain)
-        assert balance == pre_balance + amount
-
-        block = Block(transaction_set=[deposit_tx], number=blknum)
-        self.child_chain.add_block(block)
+    def submit_block(self, transactions, signer=None, force_invalid=False):
+        signer = signer or self.operator
+        blknum = self.root_chain.currentChildBlock()
+        if transactions and hasattr(transactions[0], "outputs"):
+            block = BlockV2(transactions, number=blknum)
+        else:
+            block = Block(transactions, number=blknum)
+        block.sign(signer.key)
+        self.root_chain.submitBlock(block.root, sender=signer.key)
+        if force_invalid:
+            self.child_chain.blocks[self.child_chain.next_child_block] = block
+            self.child_chain.next_deposit_block = self.child_chain.next_child_block + 1
+            self.child_chain.next_child_block += self.child_chain.child_block_interval
+        else:
+            assert self.child_chain.add_block(block)
         return blknum
+
+
+    def deposit(self, owner, amount):
+        deposit_tx = TransactionV2(outputs=[(owner.address, amount)])
+        blknum = self.root_chain.getDepositBlockNumber()
+        self.root_chain.deposit(value=amount)
+        deposit_id = encode_utxo_id(blknum, 0, 0)
+        block = Block([deposit_tx], number=blknum)
+        self.child_chain.add_block(block)
+        return deposit_id
+
+    # def deposit_pre_morevp(self, owner, amount):
+    #     """Creates a deposit transaction for a given owner and amount.
+
+    #     Args:
+    #         owner (EthereumAccount): Account to own the deposit.
+    #         amount (int): Deposit amount.
+
+    #     Returns:
+    #         int: Unique identifier of the deposit.
+    #     """
+
+    #     deposit_tx = Transaction(0, 0, 0,
+    #                              0, 0, 0,
+    #                              NULL_ADDRESS,
+    #                              owner.address, amount,
+    #                              NULL_ADDRESS, 0)
+
+    #     blknum = self.root_chain.getDepositBlock()
+    #     pre_balance = self.get_balance(self.root_chain)
+    #     self.root_chain.deposit(value=amount, sender=owner.key)
+    #     balance = self.get_balance(self.root_chain)
+    #     assert balance == pre_balance + amount
+
+    #     block = Block(transaction_set=[deposit_tx], number=blknum)
+    #     self.child_chain.add_block(block)
+    #     return blknum
 
     def deposit_token(self, owner, token, amount):
         """Mints, approves and deposits token for given owner and amount
@@ -134,17 +187,13 @@ class TestingLanguage(object):
             int: Unique identifier of the deposit.
         """
 
-        deposit_tx = Transaction(0, 0, 0,
-                                 0, 0, 0,
-                                 token.address,
-                                 owner.address, amount,
-                                 NULL_ADDRESS, 0)
+        deposit_tx = TransactionV2(outputs=[(owner.address, amount, token.address)])
 
         token.mint(owner.address, amount)
         self.ethtester.chain.mine()
         token.approve(self.root_chain.address, amount, sender=owner.key)
         self.ethtester.chain.mine()
-        blknum = self.root_chain.getDepositBlock()
+        blknum = self.root_chain.getDepositBlockNumber()
         pre_balance = self.get_balance(self.root_chain, token)
         self.root_chain.depositFrom(token.address, amount, sender=owner.key)
         balance = self.get_balance(self.root_chain, token)
@@ -154,61 +203,45 @@ class TestingLanguage(object):
         self.child_chain.add_block(block)
         return encode_utxo_id(blknum, 0, 0)
 
-    def spend_utxo(self, utxo_id, new_owner, amount, signer, force_invalid=False, auto_confirm=True):
-        """Creates a spending transaction and inserts it into the chain.
+    # def spend_utxo(self, utxo_id, new_owner, amount, signer, force_invalid=False, auto_confirm=True):
+    #     """Creates a spending transaction and inserts it into the chain.
 
-        Args:
-            utxo_id (int): Identifier of the UTXO to spend.
-            new_owner (EthereumAccount): Account to own the output of this spend.
-            amount (int): Amount to spend.
-            signer (EthereumAccount): Account to sign this transaction.
-            force_invalid (Bool) : Skip validity checks
-            auto_confirm (Bool) : Generate confirmation sig when mining block.
+    #     Args:
+    #         utxo_id (int): Identifier of the UTXO to spend.
+    #         new_owner (EthereumAccount): Account to own the output of this spend.
+    #         amount (int): Amount to spend.
+    #         signer (EthereumAccount): Account to sign this transaction.
+    #         force_invalid (Bool) : Skip validity checks
+    #         auto_confirm (Bool) : Generate confirmation sig when mining block.
 
-        Returns:
-            int: Unique identifier of the spend.
-        """
+    #     Returns:
+    #         int: Unique identifier of the spend.
+    #     """
 
-        utxo = self.child_chain.get_transaction(utxo_id)
-        spend_tx = Transaction(*decode_utxo_id(utxo_id),
-                               0, 0, 0,
-                               utxo.cur12,
-                               new_owner.address, amount,
-                               NULL_ADDRESS, 0)
-        spend_tx.sign1(signer.key)
+    #     utxo = self.child_chain.get_transaction(utxo_id)
+    #     spend_tx = Transaction(*decode_utxo_id(utxo_id),
+    #                            0, 0, 0,
+    #                            utxo.cur12,
+    #                            new_owner.address, amount,
+    #                            NULL_ADDRESS, 0)
+    #     spend_tx.sign1(signer.key)
+    #     blknum = self.submit_block([spend_tx], force_invalid=force_invalid)
+    #     tx_id = encode_utxo_id(blknum, 0, 0)
+    #     if auto_confirm:
+    #         self.confirm_spend(tx_id, signer)
+    #     return tx_id
+
+
+    def spend_utxo(self, input_ids, keys, outputs=[], force_invalid=False):
+        inputs = [decode_utxo_id(input_id) for input_id in input_ids]
+        spend_tx = TransactionV2(inputs=inputs, outputs=outputs)
+        for i in range(0, len(inputs)):
+            spend_tx.sign(i, keys[i])
         blknum = self.submit_block([spend_tx], force_invalid=force_invalid)
-        tx_id = encode_utxo_id(blknum, 0, 0)
-        if auto_confirm:
-            self.confirm_spend(tx_id, signer)
-        return tx_id
+        spend_id = encode_utxo_id(blknum, 0, 0)
+        return spend_id
 
-    def submit_block(self, transactions, signer=None, force_invalid=False):
-        signer = signer or self.operator
-        blknum = self.root_chain.currentChildBlock()
-        block = Block(transactions, number=blknum)
-        block.sign(signer.key)
-        self.root_chain.submitBlock(block.root, sender=signer.key)
-        if force_invalid:
-            self.child_chain.blocks[self.child_chain.next_child_block] = block
-            self.child_chain.next_deposit_block = self.child_chain.next_child_block + 1
-            self.child_chain.next_child_block += self.child_chain.child_block_interval
-        else:
-            assert self.child_chain.add_block(block)
-        return blknum
 
-    def confirm_spend(self, tx_id, signer):
-        """Signs a confirmation signature for a spend.
-
-        Args:
-            tx_id (int): Identifier of the transaction.
-            signer (EthereumAccount): Account to sign this confirmation.
-        """
-
-        spend_tx = self.child_chain.get_transaction(tx_id)
-        (blknum, _, _) = decode_utxo_id(tx_id)
-        block = self.child_chain.blocks[blknum]
-        confirmation_hash = sha3(spend_tx.hash + block.root)
-        self.confirmations[tx_id] = sign(confirmation_hash, signer.key)
 
     def create_utxo(self, token=NULL_ADDRESS):
         class Utxo(object):
@@ -227,7 +260,7 @@ class TestingLanguage(object):
         else:
             deposit_id = self.deposit_token(owner, token, amount)
             token_address = token.address
-        spend_id = self.spend_utxo(deposit_id, owner, 100, owner)
+        spend_id = self.spend_utxo([deposit_id], [owner.key], [(owner.address, 100, token_address)])
         spend = self.child_chain.get_transaction(spend_id)
         return Utxo(deposit_id, owner, token_address, amount, spend, spend_id)
 
@@ -270,9 +303,15 @@ class TestingLanguage(object):
         spend_tx = self.child_chain.get_transaction(utxo_id)
         (blknum, _, _) = decode_utxo_id(utxo_id)
         block = self.child_chain.blocks[blknum]
-        proof = block.merklized_transaction_set.create_membership_proof(spend_tx.merkle_hash)
-        sigs = spend_tx.sig1 + spend_tx.sig2
-        self.root_chain.startExit(utxo_id, spend_tx.encoded, proof, sigs, sender=sender.key)
+        
+        if hasattr(spend_tx, "outputs"):
+            merkle = FixedMerkle(16, [spend_tx.encoded])
+            proof = block.merklized_transaction_set.create_membership_proof(spend_tx.encoded)
+            signatures = b"".join(spend_tx.signatures)
+        else:
+            proof = block.merklized_transaction_set.create_membership_proof(spend_tx.merkle_hash)
+            signatures = spend_tx.sig1 + spend_tx.sig2
+        self.root_chain.startExit(utxo_id, spend_tx.encoded, proof, sender=sender.key)
 
     def challenge_standard_exit(self, utxo_id, spend_id):
         """Challenges an exit with a double spend.
