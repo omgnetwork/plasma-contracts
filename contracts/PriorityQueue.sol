c8121774d54fa0fcafad8760b2a38527b92003fa
Paul Peregud <paulperegud@gmail.com>
2018-11-08 14:13:16 +0100

    split PriorityQueue into storage and library parts

--- contracts/PriorityQueue.sol
+++ contracts/PriorityQueue.sol
@@ -1,14 +1,13 @@
 pragma solidity ^0.4.0;
 
-import "./SafeMath.sol";
-
+import "./PriorityQueueLib.sol";
 
 /**
  * @title PriorityQueue
  * @dev Min-heap priority queue implementation.
  */
 contract PriorityQueue {
-    using SafeMath for uint256;
+    using PriorityQueueLib for PriorityQueueLib.Queue;
 
     /* 
      *  Storage
@@ -24,39 +23,39 @@ contract PriorityQueue {
      */
 
     modifier onlyOwner() {
-        require(msg.sender == owner);
+        require(queue.isOwner());
         _;
     }
 
+    /*
+     *  Storage
+     */
+
+    PriorityQueueLib.Queue queue;
 
     /*
-     * Constructor
+     *  Public functions
      */
 
     constructor()
         public
     {
-        owner = msg.sender;
-        heapList = [0];
-        currentSize = 0;
+        queue.init();
     }
 
-
-    /*
-     * Internal functions
-     */
-
-    /**
-     * @dev Inserts an element into the priority queue.
-     * @param _element Integer to insert.
-     */
-    function insert(uint256 _element) 
-        public
+    function insert(uint256 _element)
         onlyOwner
+        public
     {
-        heapList.push(_element);
-        currentSize = currentSize.add(1);
-        _percUp(currentSize);
+        queue.insert(_element);
+    }
+
+    function minChild(uint256 i)
+        public
+        view
+        returns (uint256)
+    {
+        return queue.minChild(i);
     }
 
     /**
@@ -68,7 +67,7 @@ contract PriorityQueue {
         view
         returns (uint256)
     {
-        return heapList[1];
+        return queue.getMin();
     }
 
     /**
@@ -77,75 +76,10 @@ contract PriorityQueue {
      */
     function delMin()
         public
-        onlyOwner
-        returns (uint256)
-    {
-        uint256 retVal = heapList[1];
-        heapList[1] = heapList[currentSize];
-        delete heapList[currentSize];
-        currentSize = currentSize.sub(1);
-        _percDown(1);
-        heapList.length = heapList.length.sub(1);
-        return retVal;
-    }
-
-
-    /*
-     * Private functions
-     */
-
-    /**
-     * @dev Determines the minimum child of a given node in the tree.
-     * @param _index Index of the node in the tree.
-     * @return The smallest child node.
-     */
-    function _minChild(uint256 _index)
-        private
         view
         returns (uint256)
     {
-        if (_index.mul(2).add(1) > currentSize) {
-            return _index.mul(2);
-        } else {
-            if (heapList[_index.mul(2)] < heapList[_index.mul(2).add(1)]) {
-                return _index.mul(2);
-            } else {
-                return _index.mul(2).add(1);
-            }
-        }
+        return queue.delMin();
     }
 
-    /**
-     * @dev Bubbles the element at some index up.
-     */
-    function _percUp(uint256 _index)
-        private
-    {
-        uint256 index = _index;
-        uint256 j = index;
-        uint256 newVal = heapList[index];
-        while (newVal < heapList[index.div(2)]) {
-            heapList[index] = heapList[index.div(2)];
-            index = index.div(2);
-        }
-        if (index != j) heapList[index] = newVal;
-    }
-
-    /**
-     * @dev Bubbles the element at some index down.
-     */
-    function _percDown(uint256 _index)
-        private
-    {
-        uint256 index = _index;
-        uint256 j = index;
-        uint256 newVal = heapList[index];
-        uint256 mc = _minChild(index);
-        while (mc <= currentSize && newVal > heapList[mc]) {
-            heapList[index] = heapList[mc];
-            index = mc;
-            mc = _minChild(index);
-        }
-        if (index != j) heapList[index] = newVal;
-    }
 }
---
+++ contracts/PriorityQueueLib.sol
@@ -0,0 +1,121 @@
+pragma solidity ^0.4.0;
+
+import "./SafeMath.sol";
+
+/**
+ * @title PriorityQueueLib
+ * @dev A priority queue library implementation
+ */
+library PriorityQueueLib {
+    using SafeMath for uint256;
+
+    /*
+     *  Storage definition
+     */
+    struct Queue {
+        address owner;
+        uint256[] heapList;
+        uint256 currentSize;
+    }
+
+    /*
+     *  Public functions
+     */
+
+    function isOwner(Queue storage self)
+        internal
+        returns (bool)
+    {
+        return msg.sender == self.owner;
+    }
+
+    function init(Queue storage self)
+        internal
+    {
+        self.owner = msg.sender;
+        self.heapList = [0];
+        self.currentSize = 0;
+    }
+
+    function insert(Queue storage self, uint256 k)
+        internal
+    {
+        self.heapList.push(k);
+        self.currentSize = self.currentSize.add(1);
+        percUp(self, self.currentSize);
+    }
+
+    function minChild(Queue storage self, uint256 i)
+        view
+        internal
+        returns (uint256)
+    {
+        if (i.mul(2).add(1) > self.currentSize) {
+            return i.mul(2);
+        } else {
+            if (self.heapList[i.mul(2)] < self.heapList[i.mul(2).add(1)]) {
+                return i.mul(2);
+            } else {
+                return i.mul(2).add(1);
+            }
+        }
+    }
+
+    function getMin(Queue storage self)
+        view
+        internal
+        returns (uint256)
+    {
+        return self.heapList[1];
+    }
+
+    function delMin(Queue storage self)
+        internal
+        returns (uint256)
+    {
+        uint256 retVal = self.heapList[1];
+        self.heapList[1] = self.heapList[self.currentSize];
+        delete self.heapList[self.currentSize];
+        self.currentSize = self.currentSize.sub(1);
+        percDown(self, 1);
+        self.heapList.length = self.heapList.length.sub(1);
+        return retVal;
+    }
+
+    function getCurrentSize(Queue storage self)
+        internal
+        returns (uint256)
+    {
+        return self.currentSize;
+    }
+
+    /*
+     *  Private functions
+     */
+    function percUp(Queue storage self, uint256 i)
+        private
+    {
+        uint256 j = i;
+        uint256 newVal = self.heapList[i];
+        while (newVal < self.heapList[i.div(2)]) {
+            self.heapList[i] = self.heapList[i.div(2)];
+            i = i.div(2);
+        }
+        if (i != j) self.heapList[i] = newVal;
+    }
+
+    function percDown(Queue storage self, uint256 i)
+        private
+    {
+        uint256 j = i;
+        uint256 newVal = self.heapList[i];
+        uint256 mc = minChild(self, i);
+        while (mc <= self.currentSize && newVal > self.heapList[mc]) {
+            self.heapList[i] = self.heapList[mc];
+            i = mc;
+            mc = minChild(self, i);
+        }
+        if (i != j) self.heapList[i] = newVal;
+    }
+}
+
