7d53ffcca9887d64c5d2ca06baef5ac96d65f857
Paul Peregud <paulperegud@gmail.com>
2018-11-28 15:35:10 +0100

    tests: minor refactoring

--- tests/contracts/rlp/test_plasma_core.py
+++ tests/contracts/rlp/test_plasma_core.py
@@ -22,11 +22,13 @@ def test_slice_signature(plasma_core_test):
 
 
 def test_get_output(plasma_core_test):
+    null = '0x0000000000000000000000000000000000000000'
     owner = b'0x82a978b3f5962a5b0957d9ee9eef472ee55b42f1'
     amount = 100
-    tx = Transaction(outputs=[(owner, '0x0000000000000000000000000000000000000000', amount)])
-    assert plasma_core_test.getOutput(tx.encoded, 0) == [owner.decode("utf-8"), '0x0000000000000000000000000000000000000000', amount]
-    assert plasma_core_test.getOutput(tx.encoded, 1) == ['0x0000000000000000000000000000000000000000', '0x0000000000000000000000000000000000000000', 0]
+    tx = Transaction(outputs=[(owner, null, amount)])
+    tx = Transaction(outputs=[(owner, null, amount)])
+    assert plasma_core_test.getOutput(tx.encoded, 0) == [owner.decode("utf-8"), null, amount]
+    assert plasma_core_test.getOutput(tx.encoded, 1) == [null, null, 0]
 
 
 def test_get_input_id(plasma_core_test):
