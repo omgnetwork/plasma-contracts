972f9b9ade32210154baac40fbb612320d635b43
Paul Peregud <paulperegud@gmail.com>
2019-01-08 12:31:26 +0100

    fix: emit non-zero inFlightExitId during finalization

--- contracts/RootChain.sol
+++ contracts/RootChain.sol
@@ -1138,7 +1138,7 @@ contract RootChain {
             transferAmount = piggybackBond;
             if ((i < 4 && inputsExitable) || (i >= 4 && !inputsExitable)) {
                 transferAmount += output.amount;
-                emit InFlightExitFinalized(uint192(0), i);
+                emit InFlightExitFinalized(_inFlightExitId, i);
             }
             output.owner.transfer(transferAmount);
         }
