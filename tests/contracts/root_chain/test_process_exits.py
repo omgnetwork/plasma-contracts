5b8b6b51e20a770f3c13a53a5c1f7d281d5f6c0f
pthomalla <pawel.thomalla@imapp.pl>
2019-02-21 12:06:30 +0100

    test: events ExitStarted and ExitFinalized

--- tests/contracts/root_chain/test_process_exits.py
+++ tests/contracts/root_chain/test_process_exits.py
@@ -11,11 +11,16 @@ def test_process_exits_standard_exit_should_succeed(testlang):
     spend_id = testlang.spend_utxo([deposit_id], [owner.key], [(owner.address, NULL_ADDRESS, amount)])
 
     pre_balance = testlang.get_balance(owner)
+    testlang.flush_events()
 
     testlang.start_standard_exit(spend_id, owner.key)
-    testlang.forward_timestamp(2 * MIN_EXIT_PERIOD + 1)
+    [exit] = testlang.flush_events()
+    assert {"owner": owner.address, "_event_type": b'ExitStarted'}.items() <= exit.items()
 
+    testlang.forward_timestamp(2 * MIN_EXIT_PERIOD + 1)
     testlang.process_exits(NULL_ADDRESS, 0, 100)
+    [exit_finalized] = testlang.flush_events()
+    assert {"exitId": exit['exitId'], "_event_type": b'ExitFinalized'}.items() <= exit_finalized.items()
 
     standard_exit = testlang.get_standard_exit(spend_id)
     assert standard_exit.owner == NULL_ADDRESS_HEX
