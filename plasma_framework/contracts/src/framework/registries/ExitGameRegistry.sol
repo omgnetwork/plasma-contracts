2381c1a54fec3669537e51fced4e7ec77d9a1034
boolafish <boolafish945@gmail.com>
2019-06-07 14:45:17 +0900

    feat: registries for plasma framework
    This commit adds the registry code for exit games and vaults.
    
    issue: https://github.com/omisego/plasma-contracts/issues/117

---
+++ plasma_framework/contracts/src/framework/registries/ExitGameRegistry.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.5.0;
+
+import "../modifiers/Operated.sol";
+
+contract ExitGameRegistry is Operated {
+    mapping(uint256 => address) private _exitGames;
+    mapping(address => uint256) private _exitGameToTxType;
+
+    /**
+     * @notice Register the exit game to Plasma framework. This can be only called by contract admin.
+     * @param _txType tx type that the exit game want to register to.
+     * @param _contract Address of the exit game contract.
+     */
+    function registerExitGame(uint256 _txType, address _contract) public onlyOperator {
+        require(_exitGames[_txType] == address(0), "The tx type is already registered");
+        require(_exitGameToTxType[_contract] == 0, "The exit game contract is already registered");
+
+        _exitGames[_txType] = _contract;
+        _exitGameToTxType[_contract] = _txType;
+    }
+
+    function exitGames(uint256 _txType) public view returns (address) {
+        return _exitGames[_txType];
+    }
+
+    function exitGameToTxType(address _exitGame) public view returns (uint256) {
+        return _exitGameToTxType[_exitGame];
+    }
+}
