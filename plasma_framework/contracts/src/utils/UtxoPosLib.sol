969f88782fdeaf5ebf2cfb108ea531fea9661b2c
boolafish <boolafish945@gmail.com>
2019-06-28 15:39:42 +0900

    feat: Add UtxoPos library for related function
    Most of the original function was in 'PlasmaCore.sol'. This
    library would be used to handle utxo pos related function.
    
    PR: https://github.com/omisego/plasma-contracts/pull/139

---
+++ plasma_framework/contracts/src/utils/UtxoPosLib.sol
@@ -0,0 +1,62 @@
+pragma solidity ^0.5.0;
+
+library UtxoPosLib {
+    struct UtxoPos {
+        uint256 value;
+    }
+
+    uint256 constant internal BLOCK_OFFSET = 1000000000;
+    uint256 constant internal TX_OFFSET = 10000;
+
+    /**
+     * @notice Given an UTXO position, returns the block number.
+     * @param _utxoPos Output identifier in form of utxo position.
+     * @return The output's block number.
+     */
+    function blockNum(UtxoPos memory _utxoPos)
+        internal
+        pure
+        returns (uint256)
+    {
+        return _utxoPos.value / BLOCK_OFFSET;
+    }
+
+    /**
+     * @notice Given an UTXO position, returns the transaction index.
+     * @param _utxoPos Output identifier in form of utxo position.
+     * @return The output's transaction index.
+     */
+    function txIndex(UtxoPos memory _utxoPos)
+        internal
+        pure
+        returns (uint256)
+    {
+        return (_utxoPos.value % BLOCK_OFFSET) / TX_OFFSET;
+    }
+
+    /**
+     * @notice Given an UTXO position, returns the output index.
+     * @param _utxoPos Output identifier in form of utxo position.
+     * @return The output's index.
+     */
+    function outputIndex(UtxoPos memory _utxoPos)
+        internal
+        pure
+        returns (uint8)
+    {
+        return uint8(_utxoPos.value % TX_OFFSET);
+    }
+
+    /**
+     * @notice Given an UTXO position, returns transaction position.
+     * @param _utxoPos Output identifier in form of utxo position.
+     * @return The transaction position.
+     */
+    function txPos(UtxoPos memory _utxoPos)
+        internal
+        pure
+        returns (uint256)
+    {
+        return _utxoPos.value / TX_OFFSET;
+    }
+}
