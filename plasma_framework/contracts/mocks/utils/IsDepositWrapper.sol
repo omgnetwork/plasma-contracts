c707a3a8c5f1b9638c86e492d8348208995291d7
boolafish <boolafish945@gmail.com>
2019-07-02 18:27:56 +0900

    feat: add IsDeposit library
    This commit adds a helper library to check whether a block number
    belongs to deposit block or not.
    
    PR: https://github.com/omisego/plasma-contracts/pull/146

---
+++ plasma_framework/contracts/mocks/utils/IsDepositWrapper.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.5.0;
+
+import "../../src/utils/IsDeposit.sol";
+
+contract IsDepositWrapper {
+    using IsDeposit for IsDeposit.Predicate;
+
+    IsDeposit.Predicate isDeposit;
+
+    constructor(uint256 _childBlockInterval) public {
+        isDeposit = IsDeposit.Predicate(_childBlockInterval);
+    }
+
+    function test(uint256 _blockNum) public view returns (bool) {
+        return isDeposit.test(_blockNum);
+    }
+}
