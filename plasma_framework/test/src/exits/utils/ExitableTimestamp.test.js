7cf2cd020c927dc9ca1b6380eee8eb49f49c00b3
boolafish <boolafish945@gmail.com>
2019-07-02 19:13:41 +0900

    feat: add ExitableTimestamp library
    This commit adds ExitableTimestamp library that would
    do the calculation for exitable time logic.
    
    PR: https://github.com/omisego/plasma-contracts/pull/146

---
+++ plasma_framework/test/src/exits/utils/ExitableTimestamp.test.js
@@ -0,0 +1,40 @@
+const ExitableTimestamp = artifacts.require('ExitableTimestampWrapper');
+
+const { BN, time } = require('openzeppelin-test-helpers');
+const { expect } = require('chai');
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+contract('ExitableTimestamp', () => {
+    const MIN_EXIT_PERIOD = 60 * 60 * 24 * 7; // 1 week
+
+    before('setup', async () => {
+        this.contract = await ExitableTimestamp.new(MIN_EXIT_PERIOD);
+    });
+
+    describe('calculate', () => {
+        it('should get the correct exit timestamp for deposit tx', async () => {
+            const latestTimestamp = await time.latest();
+            await sleep(2000);
+            expect(await this.contract.calculate(latestTimestamp, MIN_EXIT_PERIOD, true))
+                .to.be.bignumber.equal(latestTimestamp.add(new BN(MIN_EXIT_PERIOD)));
+        });
+
+        it('should get the correct exit timestamp for non deposit tx whose age is older than MIN_EXIT_PERIOD', async () => {
+            const latestTimestamp = await time.latest();
+            const oldTimestamp = 0;
+            expect(await this.contract.calculate(latestTimestamp, oldTimestamp, false))
+                .to.be.bignumber.equal(latestTimestamp.add(new BN(MIN_EXIT_PERIOD)));
+        });
+
+        it('should get the correct exit timestamp for non deposit tx whose age is younger than MIN_EXIT_PERIOD', async () => {
+            const latestTimestamp = await time.latest();
+            const youngTimestamp = latestTimestamp - 15;
+            expect(await this.contract.calculate(latestTimestamp, youngTimestamp, false))
+                .to.be.bignumber.equal(new BN(youngTimestamp + 2 * MIN_EXIT_PERIOD));
+        });
+    });
+});
