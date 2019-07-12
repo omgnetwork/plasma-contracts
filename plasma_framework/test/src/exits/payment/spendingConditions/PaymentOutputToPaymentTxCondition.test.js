fb49e442cef506bd4f4a54f9efbde5951199d136
boolafish <boolafish945@gmail.com>
2019-07-12 15:33:02 +0900

    feat: add PaymentOutputToPaymentTxCondition
    This commit adds spending condition that checks payment output type
    consumes in payment tx.

---
+++ plasma_framework/test/src/exits/payment/spendingConditions/PaymentOutputToPaymentTxCondition.test.js
@@ -0,0 +1,117 @@
+const PaymentOutputToPaymentTxCondition = artifacts.require('PaymentOutputToPaymentTxCondition');
+
+const { constants, expectRevert } = require('openzeppelin-test-helpers');
+const { expect } = require('chai');
+
+const { PaymentTransactionOutput, PaymentTransaction } = require('../../../../helpers/transaction.js');
+const { addressToOutputGuard } = require('../../../../helpers/utils.js');
+const { hashTx } = require('../../../../helpers/paymentEip712.js');
+const { buildUtxoPos } = require('../../../../helpers/utxoPos.js');
+const { sign } = require('../../../../helpers/sign.js');
+
+contract('PaymentOutputToPaymentTxCondition', ([richFather]) => {
+    const ETH = constants.ZERO_ADDRESS;
+    const alicePrivateKey = '0x7151e5dab6f8e95b5436515b83f423c4df64fe4c6149f864daa209b26adb10ca';
+    let alice;
+
+    before('setup alice account with custom private key', async () => {
+        const password = 'password1234';
+        alice = await web3.eth.personal.importRawKey(alicePrivateKey, password);
+        alice = web3.utils.toChecksumAddress(alice);
+        web3.eth.personal.unlockAccount(alice, password, 3600);
+        web3.eth.sendTransaction({
+            to: alice,
+            from: richFather,
+            value: web3.utils.toWei('1', 'ether'),
+        });
+    });
+
+    beforeEach('setup contracts', async () => {
+        this.dummyFramework = constants.ZERO_ADDRESS;
+        this.condition = await PaymentOutputToPaymentTxCondition.new(this.dummyFramework);
+    });
+
+    describe('verify', () => {
+        it('should fail when consume tx is not type of payment tx', async () => {
+            const outputGuard = addressToOutputGuard(alice);
+            const output = new PaymentTransactionOutput(
+                1000, outputGuard, ETH,
+            );
+            const utxoPos = buildUtxoPos(100, 0, 1);
+            const nonUsingOutputId = '0x';
+            const inputIndex = 0;
+            const wrongTxType = 2;
+            const tx = new PaymentTransaction(wrongTxType, [utxoPos], [output]);
+            const txBytes = web3.utils.bytesToHex(tx.rlpEncoded());
+            await expectRevert(
+                this.condition.verify(
+                    outputGuard, utxoPos, nonUsingOutputId,
+                    txBytes, inputIndex, '0x',
+                ),
+                'The consume tx is not of payment tx type',
+            );
+        });
+
+        it('should fail when consume tx does not point to the utxo pos in input', async () => {
+            const outputGuard = addressToOutputGuard(alice);
+            const output = new PaymentTransactionOutput(
+                1000, outputGuard, ETH,
+            );
+            const utxoPos = buildUtxoPos(100, 0, 1);
+            const wrongUtxoPosInTx = utxoPos + 1000;
+            const nonUsingOutputId = '0x';
+            const inputIndex = 0;
+            const tx = new PaymentTransaction(1, [wrongUtxoPosInTx], [output]);
+            const txBytes = web3.utils.bytesToHex(tx.rlpEncoded());
+            await expectRevert(
+                this.condition.verify(
+                    outputGuard, utxoPos, nonUsingOutputId,
+                    txBytes, inputIndex, '0x',
+                ),
+                'The consume tx does not consume the output with such utxo pos',
+            );
+        });
+
+        it('should fail when consume tx not correctly signed by the input owner', async () => {
+            const outputGuard = addressToOutputGuard(alice);
+            const output = new PaymentTransactionOutput(
+                1000, outputGuard, ETH,
+            );
+            const utxoPos = buildUtxoPos(100, 0, 1);
+            const nonUsingOutputId = '0x';
+            const inputIndex = 0;
+            const tx = new PaymentTransaction(1, [utxoPos], [output]);
+            const txBytes = web3.utils.bytesToHex(tx.rlpEncoded());
+            const txHash = hashTx(tx, this.dummyFramework);
+            const wrongPrivateKey = `0x${Array(64).fill(1).join('')}`;
+            const wrongSignature = sign(txHash, wrongPrivateKey);
+            await expectRevert(
+                this.condition.verify(
+                    outputGuard, utxoPos, nonUsingOutputId,
+                    txBytes, inputIndex, wrongSignature,
+                ),
+                'Tx not correctly signed',
+            );
+        });
+
+        it('should return true when all verification passes', async () => {
+            const outputGuard = addressToOutputGuard(alice);
+            const output = new PaymentTransactionOutput(
+                1000, outputGuard, ETH,
+            );
+            const utxoPos = buildUtxoPos(100, 0, 1);
+            const nonUsingOutputId = '0x';
+            const inputIndex = 0;
+            const tx = new PaymentTransaction(1, [utxoPos], [output]);
+            const txBytes = web3.utils.bytesToHex(tx.rlpEncoded());
+            const txHash = hashTx(tx, this.dummyFramework);
+            const signature = sign(txHash, alicePrivateKey);
+
+            const result = await this.condition.verify(
+                outputGuard, utxoPos, nonUsingOutputId,
+                txBytes, inputIndex, signature,
+            );
+            expect(result).to.be.true;
+        });
+    });
+});
