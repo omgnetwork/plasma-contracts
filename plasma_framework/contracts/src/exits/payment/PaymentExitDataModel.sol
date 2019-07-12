bfcb5acf18bdc6231a1f11ab262b92a9c14a2698
boolafish <boolafish945@gmail.com>
2019-07-12 15:38:29 +0900

    feat: add challengeStandardExit
    This commit adds the contract code for challenge standard exit. It also
    makes the previous start standard exit uses 'output guard'.

--- plasma_framework/contracts/src/exits/payment/PaymentExitDataModel.sol
+++ plasma_framework/contracts/src/exits/payment/PaymentExitDataModel.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 library PaymentExitDataModel {
     struct StandardExit {
         bool exitable;
-        uint192 position;
+        bytes32 outputRelatedDataHash;
         address token;
         address payable exitTarget;
         uint256 amount;
