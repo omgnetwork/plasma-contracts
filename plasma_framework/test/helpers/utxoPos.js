31ce31e084b392ec0193cc06bd73100ae4c8213d
boolafish <boolafish945@gmail.com>
2019-07-12 11:49:25 +0900

    feat: add PaymentEip712Lib for supporting EIP-712
    This commits ports the origin Eip712StructHash implementation to
    PlasmaFramework. Some difference is in Transaction, we add a field
    'txType' and in Output, instead of 'owner' which is an address, change
    it to bytes32 to be competible with outputGuard.
    
    - EIP-712: https://git.io/fjP9r
    - previous EIP712 change: https://git.io/fjP9o

---
+++ plasma_framework/test/helpers/utxoPos.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+
+function buildUtxoPos(blockNum, txIndex, outputIndex) {
+    const BLOCK_OFFSET = 1000000000;
+    const TX_OFFSET = 10000;
+    return blockNum * BLOCK_OFFSET + txIndex * TX_OFFSET + outputIndex;
+}
+
+class UtxoPos {
+    constructor(utxoPos) {
+        const BLOCK_OFFSET = 1000000000;
+        const TX_OFFSET = 10000;
+
+        this.utxoPos = utxoPos;
+        this.blockNum = Math.floor(this.utxoPos / BLOCK_OFFSET);
+        this.txIndex = Math.floor((this.utxoPos % BLOCK_OFFSET) / TX_OFFSET);
+        this.outputIndex = this.utxoPos % TX_OFFSET;
+
+        assert(this.outputIndex < 256, 'output index should be less then 2^8');
+    }
+}
+
+module.exports = {
+    buildUtxoPos,
+    UtxoPos,
+};
