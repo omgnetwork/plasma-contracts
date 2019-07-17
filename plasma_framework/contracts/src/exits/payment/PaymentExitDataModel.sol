124cf6ffa6d96c66d6420087fe4883e0abc55187
boolafish <boolafish945@gmail.com>
2019-07-17 15:17:06 +0900

    docs: add comment for 'outputRelatedDataHash' for StandardExit
    That usage of such field is not intuitive so provide some comments
    for that.

--- plasma_framework/contracts/src/exits/payment/PaymentExitDataModel.sol
+++ plasma_framework/contracts/src/exits/payment/PaymentExitDataModel.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.5.0;
 library PaymentExitDataModel {
     struct StandardExit {
         bool exitable;
+        // hash of verified output related data during start exit step
+        // so it does not need to re-verify those data when challanging.
+        // It would be proving data consistency between steps instead.
         bytes32 outputRelatedDataHash;
         address token;
         address payable exitTarget;
