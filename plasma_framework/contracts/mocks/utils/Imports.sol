9c7855922b6ed6ed68de037d58d5e732d6cdd15a
Kevin Sullivan <4653170+kevsul@users.noreply.github.com>
2019-08-06 18:01:38 +0100

    refactor: rename BadERC20, remove GoodERC20

---
+++ plasma_framework/contracts/mocks/utils/Imports.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.5.0;
+
+// Import contracts from third party Solidity libraries to make them available in tests.
+
+import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
