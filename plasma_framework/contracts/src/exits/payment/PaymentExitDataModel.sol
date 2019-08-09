daeaf050ad4e7da0e0b2c6fe623c82739070b3ab
boolafish <boolafish945@gmail.com>
2019-08-09 23:29:08 +0900

    docs: remove doc on 'exit' bits for 'exitMap'
    In previous desin, isExit is used to cross check SE and IFE.
    Since we are not cross checking anymore, remove doc for that as
    we will not be using it.

--- plasma_framework/contracts/src/exits/payment/PaymentExitDataModel.sol
+++ plasma_framework/contracts/src/exits/payment/PaymentExitDataModel.sol
@@ -23,12 +23,13 @@ library PaymentExitDataModel {
 
     struct InFlightExit {
         uint256 exitStartTimestamp;
-        // exit map stores piggybacks and finalized exits
-        // bit 255 is set only when ife has finalized
-        // input is piggybacked only when input number bit is set
-        // output is piggybacked only when MAX_INPUTS + output number bit is set
-        // input is exited only when 2 * MAX_INPUTS + input number bit is set
-        // output is exited only when 3 * MAX_INPUTS + output number bit is set
+
+        /**
+         * exit map stores piggybacks and finalized exits
+         * bit 255 is set only when in-flight exit has finalized
+         * right most 0 ~ MAX_INPUT bits is flagged when input is piggybacked
+         * right most MAX_INPUT ~ MAX_INPUT + MAX_OUTPUT bits is flagged when output is piggybacked
+         */
         uint256 exitMap;
         uint256 position;
         PaymentOutputModel.Output[MAX_INPUT_NUM] inputs;
