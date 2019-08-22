fdd5297bef62a1bff81288bbbe06744b0401aea7
Kendrick Tan <kendricktan0814@gmail.com>
2019-08-22 12:12:29 +0700

    5_output authority address

--- migrations/5_output_results.js
+++ migrations/5_output_results.js
@@ -5,6 +5,6 @@ module.exports = async function (deployer, network, accounts) {
   console.log(JSON.stringify({
     contract_addr: RootChain.address,
     txhash_contract: RootChain.network.transactionHash,
-    authority_addr: process.env.AUTHORITY_ADDRESS
+    authority_addr: process.env.AUTHORITY_ADDRESS || accounts[1]
   }))
 }
