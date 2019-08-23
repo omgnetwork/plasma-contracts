65d0f9757201122250a4c1e84d6b7c49bc15d0e3
Kendrick Tan <kendricktan0814@gmail.com>
2019-08-23 14:20:13 +0700

    removed code duplication

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     "env": {
-        "browser": true,
+        "node": true,
         "es6": true
     },
     "extends": "eslint:recommended",
--- migrations/1_check_authority_address.js
+++ migrations/1_check_authority_address.js
@@ -3,23 +3,33 @@ const RootChain = artifacts.require('./RootChain.sol')
 
 
 module.exports = async function (deployer, network, accounts) {
-  if (process.env.AUTHORITY_PRIVATEKEY && process.env.DEPLOYER_PRIVATEKEY) {
-    // If we're deploying via infura
-    const deployerAddress = process.env.DEPLOYER_ADDRESS || accounts[0]
-    const authorityAddress = process.env.AUTHORITY_ADDRESS || accounts[1]
+  // Get the deployer address
+  const deployerAddress = (process.env.DEPLOYER_PASSPHRASE) ?
+    await RootChain.web3.eth.personal.newAccount(process.env.DEPLOYER_PASSPHRASE) :
+    (process.env.DEPLOYER_ADDRESS || accounts[0])
 
+  // Get the authority address
+  const authorityAddress = (process.env.AUTHORITY_PASSPHRASE) ?
+    await RootChain.web3.eth.personal.newAccount(process.env.AUTHORITY_PASSPHRASE) :
+    (process.env.AUTHORITY_ADDRESS || accounts[1])
+
+  // Throws an error if the nonce of the authority address is > 0
+  const throwIfAuthorityNonceGtZero = async () => {
     // If using an existing authority address, its nonce must be 0. Abort if it's not.
     const authorityNonce = await RootChain.web3.eth.getTransactionCount(authorityAddress)
     if (authorityNonce !== 0) {
       throw new Error(`Authority address ${authorityAddress} nonce is not 0. Aborting...`)
     }
+  }
 
+  // Funds the authority account
+  const autoFundAuthorityAccount = async () => {
     // Check if authorithy address has enough funds, if not auto fund it
     const authorithyWeiCount = await RootChain.web3.eth.getBalance(authorityAddress)
 
     // Fund authority if it has 0 funds
-    if (parseInt(authorithyWeiCount) === 0 ) {
-       // Funds authority address
+    if (parseInt(authorithyWeiCount) === 0) {
+      // Funds authority address
       console.log(`Funding authority address ${authorityAddress}`)
       await RootChain.web3.eth.sendTransaction({
         from: deployerAddress,
@@ -28,39 +38,31 @@ module.exports = async function (deployer, network, accounts) {
       })
       console.log('Successfully funded authority address.')
     }
+  }
+
+  // If user wants to deploy via infura
+  if (process.env.AUTHORITY_PRIVATEKEY && process.env.DEPLOYER_PRIVATEKEY) {
+    await throwIfAuthorityNonceGtZero()
+    await autoFundAuthorityAccount()
   } else if (process.env.USE_EXISTING_AUTHORITY_ADDRESS) {
-    // If using an existing authority address, its nonce must be 0. Abort if it's not.
-    const authorityNonce = await RootChain.web3.eth.getTransactionCount(process.env.AUTHORITY_ADDRESS)
-    if (authorityNonce !== 0) {
-      throw new Error(`Authority address ${process.env.AUTHORITY_ADDRESS} nonce is not 0. Aborting...`)
-    }
+    await throwIfAuthorityNonceGtZero()
   } else {
     // Check that AUTHORITY_PASSPHRASE is set
     if (!process.env.AUTHORITY_PASSPHRASE) {
       throw new Error("Can't create new Authority address without AUTHORITY_PASSPHRASE")
     }
 
-    // Create a new authority address
-    const authorityAddress = await RootChain.web3.eth.personal.newAccount(process.env.AUTHORITY_PASSPHRASE)
-
-    // If no DEPLOYER_ADDRESS is set default to using accounts[0]
-    const DEPLOYER_ADDRESS = process.env.DEPLOYER_ADDRESS || accounts[0]
-
     // Unlock the deployer account if necessary
     if (process.env.DEPLOYER_PASSPHRASE) {
       await RootChain.web3.eth.personal.unlockAccount(
-        DEPLOYER_ADDRESS,
+        deployerAddress,
         process.env.DEPLOYER_PASSPHRASE,
         10
       )
     }
 
     // Fund the authority address
-    await RootChain.web3.eth.sendTransaction({
-      from: DEPLOYER_ADDRESS,
-      to: authorityAddress,
-      value: process.env.AUTHORITY_ADDRESS_INITIAL_AMOUNT || 1e18
-    })
+    await autoFundAuthorityAccount()
 
     // Save the authority address to use later
     process.env.AUTHORITY_ADDRESS = authorityAddress
