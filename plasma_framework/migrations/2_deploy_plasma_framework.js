c27b9cef1f11a3cb88fd995231f9247a631b7fe4
Kendrick Tan <kendricktan0814@gmail.com>
2019-09-23 16:05:52 +0700

    init authority with authority address

--- plasma_framework/migrations/2_deploy_plasma_framework.js
+++ plasma_framework/migrations/2_deploy_plasma_framework.js
@@ -1,7 +1,6 @@
 const PlasmaFramework = artifacts.require('PlasmaFramework');
 
 module.exports = async (deployer) => {
-    const ETH_VAULT_NUMBER = 1; // Value Number
     const MIN_EXIT_PERIOD = 60 * 60 * 24 * 7; // 1 week
     const INITIAL_IMMUNE_VAULTS = 2; // ETH and ERC20 vault
     const INITIAL_IMMUNE_EXIT_GAMES = 1; // 1 for PaymentExitGame
@@ -15,5 +14,5 @@ module.exports = async (deployer) => {
     );
 
     const plasmaFramework = await PlasmaFramework.deployed();
-    await plasmaFramework.initAuthority();
+    await plasmaFramework.initAuthority({ from: global.authorityAddress });
 };
