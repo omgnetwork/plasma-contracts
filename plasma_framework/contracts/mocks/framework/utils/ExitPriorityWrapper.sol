25e2e1c988c7ce3350699da8c075865dde4126e1
Piotr Żelazko <piotr.zelazko@imapp.pl>
2019-09-24 18:46:22 +0200

    feat: return to exit priority schema from RootChain

--- plasma_framework/contracts/mocks/framework/utils/ExitPriorityWrapper.sol
+++ plasma_framework/contracts/mocks/framework/utils/ExitPriorityWrapper.sol
@@ -4,11 +4,15 @@ import "../../../src/framework/utils/ExitPriority.sol";
 import "../../../src/utils/TxPosLib.sol";
 
 contract ExitPriorityWrapper {
-    function computePriority(uint64 exitableAt, uint256 txPos, uint64 nonce) public pure returns (uint256) {
-        return ExitPriority.computePriority(exitableAt, TxPosLib.TxPos(txPos), nonce);
+    function computePriority(uint64 exitableAt, uint256 txPos, uint160 exitId) public pure returns (uint256) {
+        return ExitPriority.computePriority(exitableAt, TxPosLib.TxPos(txPos), exitId);
     }
 
     function parseExitableAt(uint256 priority) public pure returns (uint64) {
         return ExitPriority.parseExitableAt(priority);
+    }
+
+    function parseExitId(uint256 priority) public pure returns (uint160) {
+        return ExitPriority.parseExitId(priority);
     }
 }
