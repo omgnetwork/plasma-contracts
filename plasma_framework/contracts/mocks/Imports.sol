1135c99486674ebed63e8f40206cfb036000e6d5
boolafish <boolafish945@gmail.com>
2019-09-24 14:05:57 +0900

    chore: turn on optimizer + reenable e2e tests
    Previsouly the e2e tests cannot be run due to gas limit on contract deployment. With optimizer
    turned on, it can be deployed within gas limit.
    
    Also, with latest compiler (0.5.11) it can compile without the previous issue tested in 0.5.8:
    https://github.com/omisego/plasma-contracts/issues/286

--- plasma_framework/contracts/mocks/Imports.sol
+++ plasma_framework/contracts/mocks/Imports.sol
@@ -3,3 +3,7 @@ pragma solidity ^0.5.0;
 // Import contracts from third party Solidity libraries to make them available in tests.
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+
+contract Import {
+    // dummy empty contract to allow the compiler not always trying to re-compile this file.
+}
