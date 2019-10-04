380f6f49a029a8b53474dd6b2b0122548f200762
boolafish <boolafish945@gmail.com>
2019-10-04 11:11:53 +0900

    chore: output 'plasma_framework_tx_hash' and 'authority_address' in migration
    elixir-omg need the output to run their service

--- plasma_framework/migrations/6_output_results.js
+++ plasma_framework/migrations/6_output_results.js
@@ -11,9 +11,11 @@ module.exports = async (_) => {
     const paymentExitGame = await plasmaFramework.exitGames(config.registerKeys.txTypes.payment);
 
     console.log(JSON.stringify({
+        plasma_framework_tx_hash: `${PlasmaFramework.network.transactionHash}`.toLowerCase(),
         plasma_framework: `${plasmaFramework.address}`.toLowerCase(),
         eth_vault: `${ethVault}`.toLowerCase(),
         erc20_vault: `${erc20Vault}`.toLowerCase(),
         payment_exit_game: `${paymentExitGame}`.toLowerCase(),
+        authority_address: `${global.authorityAddress}`.toLowerCase(),
     }));
 };
