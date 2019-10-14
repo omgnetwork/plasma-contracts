61e3c77a4ccc634ed2df6f8da927e5b662bdd363
Pawel Gebal <pawel.gebal@imapp.pl>
2019-10-14 14:54:56 +0200

    fix: do not treat empty inputs in withdraw data as utxos to spend when processing in-flight exit

--- plasma_framework/contracts/mocks/python_tests_wrappers/RLPTest.sol
+++ plasma_framework/contracts/mocks/python_tests_wrappers/RLPTest.sol
@@ -10,7 +10,7 @@ import "../../src/utils/RLP.sol";
 contract RLPTest {
     function eight(bytes memory tx_bytes)
         public
-        view
+        pure
         returns (uint256, address, address)
     {
         RLP.RLPItem[] memory txList = RLP.toList(RLP.toRLPItem(tx_bytes));
@@ -23,7 +23,7 @@ contract RLPTest {
 
     function eleven(bytes memory tx_bytes)
         public
-        view
+        pure
         returns (uint256, address, address, address)
     {
         RLP.RLPItem[] memory  txList = RLP.toList(RLP.toRLPItem(tx_bytes));
