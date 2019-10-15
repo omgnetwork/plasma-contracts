e04663f36bba4e9130b7916353794a743abec08a
cduangprom <50566537+cduangprom@users.noreply.github.com>
2019-10-15 09:34:02 +0700

    Minor edit

--- plasma_framework/contracts/src/exits/interfaces/IOutputGuardHandler.sol
+++ plasma_framework/contracts/src/exits/interfaces/IOutputGuardHandler.sol
@@ -4,19 +4,19 @@ pragma experimental ABIEncoderV2;
 import "../models/OutputGuardModel.sol";
 
 /**
- * @notice An interface for utils functions needed to process and get essential data from output guard field.
- * @dev This is required since there are multiple ways of using the field 'output guard'. For instance, in normal
- *      payments the output guard field holds the owner's address directly while for DEX deposits, it uses
- *      the privacy deposit mechanism and becomes hash of output type and preimage.
+ * @notice An interface for utils functions, required for processing and retrieving essential data from the `output guard` field.
+ * @dev This is required because there are multiple ways of using the `output guard` field. For example, in normal
+ *      payments the `output guard` field holds the owner's address directly, while for DEX deposits the `output guard` field uses
+ *      the privacy deposit mechanism and becomes the hash of output type and preimage.
  */
 interface IOutputGuardHandler {
     /**
-    * @notice Checks a given output guard data
+    * @notice Checks a given output guard data.
     */
     function isValid(OutputGuardModel.Data calldata object) external view returns (bool);
 
     /**
-    * @notice Gets the 'exit target' from the data set
+    * @notice Gets the 'exit target' from the data set.
     */
     function getExitTarget(OutputGuardModel.Data calldata object) external view returns (address payable);
 
