6bea20736646996081cb9d43c52ffd4f5625d0f8
cduangprom <50566537+cduangprom@users.noreply.github.com>
2019-10-15 10:30:40 +0700

    Minor edit

--- plasma_framework/contracts/src/exits/models/OutputGuardModel.sol
+++ plasma_framework/contracts/src/exits/models/OutputGuardModel.sol
@@ -4,8 +4,8 @@ pragma experimental ABIEncoderV2;
 library OutputGuardModel {
     /**
      * @dev The data structure that is being used for IOutputGuardHandler. Contains essential data related to output guard.
-     * @param guard the output guard inside an output
-     * @param preimage the original data of the output guard
+     * @param guard The output guard inside an output.
+     * @param preimage The original data of the output guard.
      */
     struct Data {
         bytes20 guard;
