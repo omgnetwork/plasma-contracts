12fde0131dd9d2c2e5308f3ad4b09fb92fe250a6
cduangprom <50566537+cduangprom@users.noreply.github.com>
2019-10-15 17:02:17 +0700

    Update PaymentInFlightExitRouterArgs.sol

--- plasma_framework/contracts/src/exits/payment/routers/PaymentInFlightExitRouterArgs.sol
+++ plasma_framework/contracts/src/exits/payment/routers/PaymentInFlightExitRouterArgs.sol
@@ -2,16 +2,16 @@ pragma solidity 0.5.11;
 
 library PaymentInFlightExitRouterArgs {
     /**
-    * @notice Wraps arguments for startInFlightExit.
-    * @param inFlightTx RLP encoded in-flight transaction.
-    * @param inputTxs Transactions that created the inputs to the in-flight transaction. In the same order as in-flight transaction inputs.
-    * @param inputTxTypes Transaction type of the input transactions.
-    * @param inputUtxosPos Utxos that represent in-flight transaction inputs. In the same order as input transactions.
-    * @param outputGuardPreimagesForInputs (Optional) Output guard pre-images for in-flight transaction inputs. Length must always match that of the inputTxs
-    * @param inputTxsInclusionProofs Merkle proofs that show the input-creating transactions are valid. In the same order as input transactions.
-    * @param inputTxsConfirmSigs (Optional) Confirm signatures for the input txs. Should be empty bytes if the input tx is MoreVP. Length must always match that of the inputTxs
-    * @param inFlightTxWitnesses Witnesses for in-flight transaction. In the same order as input transactions.
-    * @param inputSpendingConditionOptionalArgs (Optional) Additional args for the spending condition for checking inputs. Should provide empty bytes if nothing is required. Length must always match that of the inputTxs
+    * @notice Wraps arguments for startInFlightExit
+    * @param inFlightTx RLP-encoded in-flight transaction
+    * @param inputTxs Transactions that create inputs to the in-flight transaction. Occur in the same order as in-flight transaction inputs.
+    * @param inputTxTypes Transaction type of the input transactions
+    * @param inputUtxosPos UTXOs representing in-flight transaction inputs. Occur in the same order as input transactions.
+    * @param outputGuardPreimagesForInputs (Optional) Output guard preimages for in-flight transaction inputs. Length must always match the inputTxs.
+    * @param inputTxsInclusionProofs Merkle proofs showing that the input-creating transactions are valid. Occur in the same order as input transactions.
+    * @param inputTxsConfirmSigs (Optional) Confirm signatures for the input txs. Should be empty bytes, if the input tx is MoreVP. Length must always match the inputTxs.
+    * @param inFlightTxWitnesses Witnesses for in-flight transaction. Occur in the same order as input transactions.
+    * @param inputSpendingConditionOptionalArgs (Optional) Additional args for the spending condition used for checking inputs. Should provide empty bytes if nothing is required. Length must always match the inputTxs.
     */
     struct StartExitArgs {
         bytes inFlightTx;
@@ -26,9 +26,9 @@ library PaymentInFlightExitRouterArgs {
     }
 
     /**
-    * @notice Wraps arguments for piggybackInFlightExit.
-    * @param inFlightTx RLP encoded in-flight transaction.
-    * @param inputIndex Index of the input/output to piggyback on.
+    * @notice Wraps arguments for piggybackInFlightExit
+    * @param inFlightTx RLP-encoded in-flight transaction
+    * @param inputIndex Index of the input/output to piggyback on
     */
     struct PiggybackInFlightExitOnInputArgs {
         bytes inFlightTx;
@@ -36,10 +36,10 @@ library PaymentInFlightExitRouterArgs {
     }
 
     /**
-    * @notice Wraps arguments for piggybackInFlightExit.
-    * @param inFlightTx RLP encoded in-flight transaction.
-    * @param outputIndex Index of the output to piggyback on.
-    * @param outputGuardPreimage (Optional) The original data (pre-image) for the outputguard.
+    * @notice Wraps arguments for piggybackInFlightExit
+    * @param inFlightTx RLP-encoded in-flight transaction
+    * @param outputIndex Index of the output to piggyback on
+    * @param outputGuardPreimage (Optional) The original data (preimage) for the outputguard
     */
     struct PiggybackInFlightExitOnOutputArgs {
         bytes inFlightTx;
@@ -48,16 +48,16 @@ library PaymentInFlightExitRouterArgs {
     }
 
     /*
-     * @notice Wraps arguments for challenge in-flight exit not canonical.
-     * @param inFlightTx RLP encoded in-flight transaction.
-     * @param inFlightTxInputIndex Index of shared input in transaction in flight.
-     * @param competingTx RLP encoded competing transaction.
-     * @param competingTxInputIndex Index of shared input in competing transaction.
-     * @param outputGuardPreimage (Optional) Output guard preimage of the shared input.
-     * @param competingTxPos (Optional) Position of competing transaction in chain if included.
-     * @param competingTxInclusionProof (Optional) Merkle proofs that show the competing transaction was contained in chain.
-     * @param competingTxWitness Witness for competing transaction.
-     * @param competingTxConfirmSig (Optional) Confirm signature if the competing tx is of MVP protocol.
+     * @notice Wraps arguments for challenging non-canonical in-flight exits.
+     * @param inFlightTx RLP-encoded in-flight transaction
+     * @param inFlightTxInputIndex Index of shared input for transactions in flight
+     * @param competingTx RLP-encoded competing transaction
+     * @param competingTxInputIndex Index of shared input in competing transaction
+     * @param outputGuardPreimage (Optional) Output guard preimage of the shared input
+     * @param competingTxPos (Optional) Position of competing transaction in the chain, if included
+     * @param competingTxInclusionProof (Optional) Merkle proofs showing that the competing transaction was contained in chain
+     * @param competingTxWitness Witness for competing transaction
+     * @param competingTxConfirmSig (Optional) Confirm signature, if competing tx is of MVP protocol
      * @param competingTxSpendingConditionOptionalArgs (Optional) Additional arguments for the spending condition
      */
     struct ChallengeCanonicityArgs {
@@ -76,15 +76,15 @@ library PaymentInFlightExitRouterArgs {
     }
 
     /*
-     * @notice Wraps arguments for challenge in-flight exit input spent.
-     * @param inFlightTx RLP encoded in-flight transaction.
-     * @param inFlightTxInputIndex Index of input that's been spent.
-     * @param challengingTx RLP encoded challenging transaction.
-     * @param challengingTxInputIndex Index of spent input in challenging transaction.
-     * @param challengingTxWitness Witness for challenging transaction.
-     * @param inputTx RLP encoded input transaction.
-     * @param inputUtxoPos Utxo position of input transaction's output.
-     * @param spendingConditionOptionalArgs (Optional) Additional arguments for spending condition of the input transaction.
+     * @notice Wraps arguments for challenging in-flight exit input spent
+     * @param inFlightTx RLP-encoded in-flight transaction
+     * @param inFlightTxInputIndex Index of spent input
+     * @param challengingTx RLP-encoded challenging transaction
+     * @param challengingTxInputIndex Index of spent input in a challenging transaction
+     * @param challengingTxWitness Witness for challenging transactions
+     * @param inputTx RLP-encoded input transaction
+     * @param inputUtxoPos UTXO position of input transaction's output
+     * @param spendingConditionOptionalArgs (Optional) Additional arguments for the spending condition of the input transaction
      */
     struct ChallengeInputSpentArgs {
         bytes inFlightTx;
@@ -98,14 +98,14 @@ library PaymentInFlightExitRouterArgs {
     }
 
      /*
-     * @notice Wraps arguments for challenging in-flight transaction output exit.
-     * @param inFlightTx RLP encoded in-flight transaction.
-     * @param inFlightTxInclusionProof Proof that in-flight transaction is included in Plasma.
-     * @param outputUtxoPos Utxo position of challenged output.
-     * @param challengingTx RLP encoded challenging transaction.
-     * @param challengingTxInputIndex input index of challenged output in challenging transaction.
-     * @param challengingTxWitness Witness for challenging transaction.
-     * @param spendingConditionOptionalArgs (Optional) Additional data for the spending condition.
+     * @notice Wraps arguments for challenging in-flight transaction output exit
+     * @param inFlightTx RLP-encoded in-flight transaction
+     * @param inFlightTxInclusionProof Proof that an in-flight transaction is included in Plasma
+     * @param outputUtxoPos UTXO position of challenged output
+     * @param challengingTx RLP-encoded challenging transaction
+     * @param challengingTxInputIndex Input index of challenged output in a challenging transaction
+     * @param challengingTxWitness Witness for challenging transaction
+     * @param spendingConditionOptionalArgs (Optional) Additional data for the spending condition
      */
     struct ChallengeOutputSpent {
         bytes inFlightTx;
