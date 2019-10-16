eed813828e1e849eabc23c8f97ca9257b166d993
cduangprom <50566537+cduangprom@users.noreply.github.com>
2019-10-16 14:40:37 +0700

    Update PaymentOutputToPaymentTxCondition.sol

--- plasma_framework/contracts/src/exits/payment/spendingConditions/PaymentOutputToPaymentTxCondition.sol
+++ plasma_framework/contracts/src/exits/payment/spendingConditions/PaymentOutputToPaymentTxCondition.sol
@@ -30,12 +30,12 @@ contract PaymentOutputToPaymentTxCondition is ISpendingCondition {
 
     /**
      * @notice Verifies the spending condition
-     * @param inputTxBytes encoded input transaction in bytes
-     * @param outputIndex the output index of the input transaction
-     * @param inputTxPos the tx position of the input tx. (0 if in-flight)
-     * @param spendingTxBytes spending transaction in bytes
-     * @param inputIndex the input index of the spending tx that points to the output
-     * @param signature signature of the output owner
+     * @param inputTxBytes Encoded input transaction, in bytes
+     * @param outputIndex Output index of the input transaction
+     * @param inputTxPos The tx position of the input tx (0 if in-flight)
+     * @param spendingTxBytes Spending transaction, in bytes
+     * @param inputIndex Input index of the spending tx that points to the output
+     * @param signature Signature of the output owner
      */
     function verify(
         bytes calldata inputTxBytes,
@@ -51,19 +51,19 @@ contract PaymentOutputToPaymentTxCondition is ISpendingCondition {
         returns (bool)
     {
         PaymentTransactionModel.Transaction memory inputTx = PaymentTransactionModel.decode(inputTxBytes);
-        require(inputTx.txType == supportInputTxType, "The input tx is not of the supported payment tx type");
+        require(inputTx.txType == supportInputTxType, "The input tx is an unsupported payment tx type");
 
         PaymentTransactionModel.Transaction memory spendingTx = PaymentTransactionModel.decode(spendingTxBytes);
-        require(spendingTx.txType == supportSpendingTxType, "The spending tx is not of the supported payment tx type");
+        require(spendingTx.txType == supportSpendingTxType, "The spending tx is an unsupported payment tx type");
 
         UtxoPosLib.UtxoPos memory utxoPos = UtxoPosLib.build(TxPosLib.TxPos(inputTxPos), outputIndex);
         require(
             spendingTx.inputs[inputIndex] == bytes32(utxoPos.value),
-            "The spending tx does not point to the correct utxo position of the output"
+            "The spending tx is pointing to the incorrect output UTXO position"
         );
 
         address payable owner = inputTx.outputs[outputIndex].owner();
-        require(owner == ECDSA.recover(eip712.hashTx(spendingTx), signature), "Tx not correctly signed");
+        require(owner == ECDSA.recover(eip712.hashTx(spendingTx), signature), "Tx signature is incorrectly signed");
 
         return true;
     }
