398381b690fae4e2d51804b8fc536713afc83f81
cduangprom <50566537+cduangprom@users.noreply.github.com>
2019-10-17 13:15:55 +0700

    Update WireTransaction.sol

--- plasma_framework/contracts/src/transactions/WireTransaction.sol
+++ plasma_framework/contracts/src/transactions/WireTransaction.sol
@@ -4,9 +4,9 @@ import "../utils/RLP.sol";
 
 /**
  * @title WireTransaction
- * @dev Utility functions for working with transactions in wire format. This assumes our transaction would be under certain data structure limitation.
- *      Current transaction structure should be able to support Payment and DEX related transactions.
- *      If the assumption breaks, we can still upgrade to a new ExitGame with either another transaction data structure or replace this with interfaces.
+ * @dev Utility functions for working with transactions in wire format, assuming our transactions have specified data structure limitations
+ *      We assume that the current transaction structure supports transactions related to payment and DEX.
+ *      Alternatively, it's possible to upgrade to a new ExitGame, using either an alternative transaction data structure, or interfaces
  */
 library WireTransaction {
 
@@ -21,8 +21,8 @@ library WireTransaction {
     }
 
     /**
-    * @dev Returns output for transaction in wire format.
-    * Outputs is a field on the second index and should be a list where first three elements are: amount, output guard, token.
+    * @dev Returns output for transaction in wire format
+    * Outputs is a field on the second index that should be a list with the following first three elements: amount, output guard, token
     */
     function getOutput(bytes memory transaction, uint16 outputIndex) internal pure returns (Output memory) {
         RLP.RLPItem[] memory rlpTx = transaction.toRLPItem().toList();
@@ -39,8 +39,8 @@ library WireTransaction {
     }
 
     /**
-    * @dev Returns transaction type for transaction in wire format.
-    * Transaction type is the value of first field in rlp encoded list.
+    * @dev Returns a transaction type for transaction, in wire format
+    * Transaction type is the value of the first field in RLP-encoded list
     */
     function getTransactionType(bytes memory transaction) internal pure returns (uint256) {
         RLP.RLPItem[] memory rlpTx = transaction.toRLPItem().toList();
