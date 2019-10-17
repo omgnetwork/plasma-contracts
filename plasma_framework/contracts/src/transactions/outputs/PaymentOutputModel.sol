1cabb452a4aee144cb16d434b1b7bb71982f3fd7
cduangprom <50566537+cduangprom@users.noreply.github.com>
2019-10-17 13:03:11 +0700

    Update PaymentOutputModel.sol

--- plasma_framework/contracts/src/transactions/outputs/PaymentOutputModel.sol
+++ plasma_framework/contracts/src/transactions/outputs/PaymentOutputModel.sol
@@ -5,7 +5,7 @@ import "../../utils/RLP.sol";
 import "../../utils/AddressPayable.sol";
 
 /**
- * @notice Data structure and its decode function for Payment output
+ * @notice Data structure and its decode function for payment output
  */
 library PaymentOutputModel {
 
@@ -19,10 +19,10 @@ library PaymentOutputModel {
     }
 
     /**
-     * @notice Get the 'owner' from the output with the assumption of
-     *         'outputGuard' field directly holding owner's address.
-     * @dev 'outputGuard' can potentially be a hash of pre-image that holds the owner info.
-     *       This should not and cannot be handled here.
+     * @notice Retrieve the 'owner' from the output, assuming the 
+     *         'outputGuard' field directly holds the owner's address
+     * @dev It's possible that 'outputGuard' can be a hash of preimage that holds the owner information,
+     *       but this should not and cannot be handled here.
      */
     function owner(Output memory _output) internal pure returns (address payable) {
         return AddressPayable.convert(address(uint160(_output.outputGuard)));
