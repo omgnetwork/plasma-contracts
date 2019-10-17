c8d18363b31988aefe647efc4c883453da2f920d
cduangprom <50566537+cduangprom@users.noreply.github.com>
2019-10-17 11:12:33 +0700

    Update BlockController.sol

--- plasma_framework/contracts/src/framework/BlockController.sol
+++ plasma_framework/contracts/src/framework/BlockController.sol
@@ -6,13 +6,13 @@ import "./utils/Operated.sol";
 
 /**
 * @notice Controls the logic and functions for block submissions in PlasmaFramework
-* @dev We have two kinds of blocks: child block and deposit block.
-*      Each child block has an interval of 'childBlockInterval'.
+* @dev There are two types of blocks: child block and deposit block
+*      Each child block has an interval of 'childBlockInterval'
 *      The interval is preserved for deposits. Each deposit results in one deposit block.
 *      For instance, a child block would be in block 1000 and the next deposit would result in block 1001.
 *
-*      Meanwhile, block submission can only be done by the authority address.
-*      There is some limitation on the authority address, see: https://github.com/omisego/elixir-omg#managing-the-operator-address
+*      Only the authority address can perform a block submission.
+*      Details on limitations on the authority address can be found here: https://github.com/omisego/elixir-omg#managing-the-operator-address
 */
 contract BlockController is Operated, VaultRegistry {
     address public authority;
@@ -36,35 +36,35 @@ contract BlockController is Operated, VaultRegistry {
     }
 
     /**
-     * @notice Sets the operator's authority address and unlocks block submission.
-     * @dev Can be called only once, before any call to `submitBlock`.
-     * @dev All block submission then needs to be send from msg.sender address.
-     * @dev see discussion in https://github.com/omisego/plasma-contracts/issues/233
+     * @notice Sets the operator's authority address and unlocks block submission
+     * @dev Can be called only once, before any call to `submitBlock`
+     * @dev All block submission must be sent from msg.sender address
+     * @dev For more information, see https://github.com/omisego/plasma-contracts/issues/233
      */
     function initAuthority() external {
-        require(authority == address(0), "Authority address has been already set.");
+        require(authority == address(0), "Authority address already set");
         authority = msg.sender;
     }
 
     /**
-     * @notice Allows the operator to set a new authority address. This allows to implement mechanical
-     * re-org protection mechanism, explained in https://github.com/omisego/plasma-contracts/issues/118
-     * @param newAuthority address of new authority, cannot be blank.
+     * @notice Allows the operator to set a new authority address, which enables implementation of mechanical
+     * re-org protection mechanism described in https://github.com/omisego/plasma-contracts/issues/118
+     * @param newAuthority Address of new authority, which cannot be blank
      */
     function setAuthority(address newAuthority) external onlyOperator {
-        require(newAuthority != address(0), "Authority cannot be zero-address.");
+        require(newAuthority != address(0), "Authority address is required");
         authority = newAuthority;
     }
 
     /**
-     * @notice Allows the authority to submit the Merkle root of a plasma block.
+     * @notice Allows the authority to submit the Merkle root of a Plasma block
      * @dev emit BlockSubmitted event
-     * @dev Block number jumps 'childBlockInterval' per submission.
-     * @dev see discussion in https://github.com/omisego/plasma-contracts/issues/233
-     * @param _blockRoot Merkle root of the plasma block.
+     * @dev Block number jumps 'childBlockInterval' per submission
+     * @dev See discussion in https://github.com/omisego/plasma-contracts/issues/233
+     * @param _blockRoot Merkle root of the Plasma block
      */
     function submitBlock(bytes32 _blockRoot) external {
-        require(msg.sender == authority, "Can be called only by the Authority.");
+        require(msg.sender == authority, "Can be called only by the Authority");
         uint256 submittedBlockNumber = nextChildBlock;
 
         blocks[submittedBlockNumber] = BlockModel.Block({
@@ -79,10 +79,10 @@ contract BlockController is Operated, VaultRegistry {
     }
 
     /**
-     * @notice Submits a block for deposit.
-     * @dev Block number adds 1 per submission, could have at most 'childBlockInterval' deposit blocks between two child chain blocks.
-     * @param _blockRoot Merkle root of the plasma block.
-     * @return the deposit block number
+     * @notice Submits a block for deposit
+     * @dev Block number adds 1 per submission; it's possible to have at most 'childBlockInterval' deposit blocks between two child chain blocks
+     * @param _blockRoot Merkle root of the Plasma block
+     * @return The deposit block number
      */
     function submitDepositBlock(bytes32 _blockRoot) public onlyFromNonQuarantinedVault returns (uint256) {
         require(nextDeposit < childBlockInterval, "Exceeded limit of deposits per child block interval");
