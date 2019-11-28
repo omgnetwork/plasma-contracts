6b7d46a2dad28ea27c9abe143274174730ce32c0
boolafish <boolafish945@gmail.com>
2019-11-28 14:49:47 +0900

    test: add tests for isInputEmpty/isOutputEmpty
    This commit add test for new function in PaymentInFlightExitModelUtils. Also realized that the original tests
    have bug so fix those as well.

--- plasma_framework/test/src/exits/payment/PaymentInFlightExitModelUtils.test.js
+++ plasma_framework/test/src/exits/payment/PaymentInFlightExitModelUtils.test.js
@@ -3,8 +3,10 @@
 
 const PaymentInFlightExitModelUtils = artifacts.require('PaymentInFlightExitModelUtilsMock');
 
-const { expectRevert, time } = require('openzeppelin-test-helpers');
+const { expectRevert, time, constants } = require('openzeppelin-test-helpers');
 const { expect } = require('chai');
+
+const { EMPTY_BYTES_32 } = require('../../../helpers/constants.js');
 
 contract('PaymentInFlightExitModelUtils', () => {
     const MAX_INPUTS = 4;
@@ -12,13 +14,215 @@ contract('PaymentInFlightExitModelUtils', () => {
     const MIN_EXIT_PERIOD = 1000;
     const ALL_INPUTS_PIGGYBACKED = 2 ** 0 | 2 ** 1 | 2 ** 2 | 2 ** 3;
     const ALL_OUTPUTS_PIGGYBACKED = 2 ** MAX_INPUTS | 2 ** (MAX_INPUTS + 1)
-    | 2 ** (MAX_INPUTS + 2) | 2 ** 2 ** (MAX_INPUTS + 2);
+    | 2 ** (MAX_INPUTS + 2) | 2 ** (MAX_INPUTS + 3);
+
+    describe('isInputEmpty', () => {
+        it('returns true when the input is empty', async () => {
+            const modelUtils = await PaymentInFlightExitModelUtils.new(0, 0);
+
+            for (let i = 0; i < MAX_INPUTS; i++) {
+                const actual = await modelUtils.isInputEmpty(i);
+                expect(actual).to.be.true;
+            }
+        });
+
+        it('returns false when the outputId is not empty', async () => {
+            const modelUtils = await PaymentInFlightExitModelUtils.new(0, 0);
+
+            const dataWithNonEmptyOutputId = {
+                outputId: web3.utils.sha3('non empty output id'),
+                exitTarget: constants.ZERO_ADDRESS,
+                token: constants.ZERO_ADDRESS,
+                amount: 0,
+                piggybackBondSize: 0,
+            };
+
+            for (let i = 0; i < MAX_INPUTS; i++) {
+                await modelUtils.setWithdrawData('inputs', i, dataWithNonEmptyOutputId);
+                const actual = await modelUtils.isInputEmpty(i);
+                expect(actual).to.be.false;
+            }
+        });
+
+        it('returns false when the exitTarget is not empty', async () => {
+            const modelUtils = await PaymentInFlightExitModelUtils.new(0, 0);
+
+            const dataWithNonEmptyAddress = {
+                outputId: EMPTY_BYTES_32,
+                exitTarget: modelUtils.address,
+                token: constants.ZERO_ADDRESS,
+                amount: 0,
+                piggybackBondSize: 0,
+            };
+
+            for (let i = 0; i < MAX_INPUTS; i++) {
+                await modelUtils.setWithdrawData('inputs', i, dataWithNonEmptyAddress);
+                const actual = await modelUtils.isInputEmpty(i);
+                expect(actual).to.be.false;
+            }
+        });
+
+        it('returns false when the token is not empty', async () => {
+            const modelUtils = await PaymentInFlightExitModelUtils.new(0, 0);
+
+            const dataWithNonEmptyToken = {
+                outputId: EMPTY_BYTES_32,
+                exitTarget: constants.ZERO_ADDRESS,
+                token: modelUtils.address,
+                amount: 0,
+                piggybackBondSize: 0,
+            };
+
+            for (let i = 0; i < MAX_INPUTS; i++) {
+                await modelUtils.setWithdrawData('inputs', i, dataWithNonEmptyToken);
+                const actual = await modelUtils.isInputEmpty(i);
+                expect(actual).to.be.false;
+            }
+        });
+
+        it('returns false when the amount is not empty', async () => {
+            const modelUtils = await PaymentInFlightExitModelUtils.new(0, 0);
+
+            const dataWithNonEmptyAmount = {
+                outputId: EMPTY_BYTES_32,
+                exitTarget: constants.ZERO_ADDRESS,
+                token: constants.ZERO_ADDRESS,
+                amount: 1,
+                piggybackBondSize: 0,
+            };
+
+            for (let i = 0; i < MAX_INPUTS; i++) {
+                await modelUtils.setWithdrawData('inputs', i, dataWithNonEmptyAmount);
+                const actual = await modelUtils.isInputEmpty(i);
+                expect(actual).to.be.false;
+            }
+        });
+
+        it('returns false when the piggyback bond is not empty', async () => {
+            const modelUtils = await PaymentInFlightExitModelUtils.new(0, 0);
+
+            const dataWithNonEmptyPiggybackBondSize = {
+                outputId: EMPTY_BYTES_32,
+                exitTarget: constants.ZERO_ADDRESS,
+                token: constants.ZERO_ADDRESS,
+                amount: 0,
+                piggybackBondSize: 1,
+            };
+
+            for (let i = 0; i < MAX_INPUTS; i++) {
+                await modelUtils.setWithdrawData('inputs', i, dataWithNonEmptyPiggybackBondSize);
+                const actual = await modelUtils.isInputEmpty(i);
+                expect(actual).to.be.false;
+            }
+        });
+    });
+
+    describe('isOutputEmpty', () => {
+        it('returns true when the output is empty', async () => {
+            const modelUtils = await PaymentInFlightExitModelUtils.new(0, 0);
+
+            for (let i = 0; i < MAX_OUTPUTS; i++) {
+                const actual = await modelUtils.isOutputEmpty(i);
+                expect(actual).to.be.true;
+            }
+        });
+
+        it('returns false when the outputId is not empty', async () => {
+            const modelUtils = await PaymentInFlightExitModelUtils.new(0, 0);
+
+            const dataWithNonEmptyOutputId = {
+                outputId: web3.utils.sha3('non empty output id'),
+                exitTarget: constants.ZERO_ADDRESS,
+                token: constants.ZERO_ADDRESS,
+                amount: 0,
+                piggybackBondSize: 0,
+            };
+
+            for (let i = 0; i < MAX_OUTPUTS; i++) {
+                await modelUtils.setWithdrawData('outputs', i, dataWithNonEmptyOutputId);
+                const actual = await modelUtils.isOutputEmpty(i);
+                expect(actual).to.be.false;
+            }
+        });
+
+        it('returns false when the exitTarget is not empty', async () => {
+            const modelUtils = await PaymentInFlightExitModelUtils.new(0, 0);
+
+            const dataWithNonEmptyAddress = {
+                outputId: EMPTY_BYTES_32,
+                exitTarget: modelUtils.address,
+                token: constants.ZERO_ADDRESS,
+                amount: 0,
+                piggybackBondSize: 0,
+            };
+
+            for (let i = 0; i < MAX_OUTPUTS; i++) {
+                await modelUtils.setWithdrawData('outputs', i, dataWithNonEmptyAddress);
+                const actual = await modelUtils.isOutputEmpty(i);
+                expect(actual).to.be.false;
+            }
+        });
+
+        it('returns false when the token is not empty', async () => {
+            const modelUtils = await PaymentInFlightExitModelUtils.new(0, 0);
+
+            const dataWithNonEmptyToken = {
+                outputId: EMPTY_BYTES_32,
+                exitTarget: constants.ZERO_ADDRESS,
+                token: modelUtils.address,
+                amount: 0,
+                piggybackBondSize: 0,
+            };
+
+            for (let i = 0; i < MAX_OUTPUTS; i++) {
+                await modelUtils.setWithdrawData('outputs', i, dataWithNonEmptyToken);
+                const actual = await modelUtils.isOutputEmpty(i);
+                expect(actual).to.be.false;
+            }
+        });
+
+        it('returns false when the amount is not empty', async () => {
+            const modelUtils = await PaymentInFlightExitModelUtils.new(0, 0);
+
+            const dataWithNonEmptyAmount = {
+                outputId: EMPTY_BYTES_32,
+                exitTarget: constants.ZERO_ADDRESS,
+                token: constants.ZERO_ADDRESS,
+                amount: 1,
+                piggybackBondSize: 0,
+            };
+
+            for (let i = 0; i < MAX_OUTPUTS; i++) {
+                await modelUtils.setWithdrawData('outputs', i, dataWithNonEmptyAmount);
+                const actual = await modelUtils.isOutputEmpty(i);
+                expect(actual).to.be.false;
+            }
+        });
+
+        it('returns false when the piggyback bond is not empty', async () => {
+            const modelUtils = await PaymentInFlightExitModelUtils.new(0, 0);
+
+            const dataWithNonEmptyPiggybackBondSize = {
+                outputId: EMPTY_BYTES_32,
+                exitTarget: constants.ZERO_ADDRESS,
+                token: constants.ZERO_ADDRESS,
+                amount: 0,
+                piggybackBondSize: 1,
+            };
+
+            for (let i = 0; i < MAX_OUTPUTS; i++) {
+                await modelUtils.setWithdrawData('outputs', i, dataWithNonEmptyPiggybackBondSize);
+                const actual = await modelUtils.isOutputEmpty(i);
+                expect(actual).to.be.false;
+            }
+        });
+    });
 
     describe('isInputPiggybacked', () => {
         it('returns true for piggybacked input', async () => {
             this.modelUtils = await PaymentInFlightExitModelUtils.new(ALL_INPUTS_PIGGYBACKED, 0);
 
-            for (let i = 0; i++; i < MAX_INPUTS) {
+            for (let i = 0; i < MAX_INPUTS; i++) {
                 const actual = await this.modelUtils.isInputPiggybacked(i);
                 expect(actual).to.be.true;
             }
@@ -27,7 +231,7 @@ contract('PaymentInFlightExitModelUtils', () => {
         it('returns false for non-piggybacked input', async () => {
             this.modelUtils = await PaymentInFlightExitModelUtils.new(0, 0);
 
-            for (let i = 0; i++; i < MAX_INPUTS) {
+            for (let i = 0; i < MAX_INPUTS; i++) {
                 const actual = await this.modelUtils.isInputPiggybacked(i);
                 expect(actual).to.be.false;
             }
@@ -47,7 +251,7 @@ contract('PaymentInFlightExitModelUtils', () => {
         it('returns true for piggybacked output', async () => {
             this.modelUtils = await PaymentInFlightExitModelUtils.new(ALL_OUTPUTS_PIGGYBACKED, 0);
 
-            for (let i = 0; i++; i < MAX_OUTPUTS) {
+            for (let i = 0; i < MAX_OUTPUTS; i++) {
                 const actual = await this.modelUtils.isOutputPiggybacked(i);
                 expect(actual).to.be.true;
             }
@@ -56,7 +260,7 @@ contract('PaymentInFlightExitModelUtils', () => {
         it('returns false for non-piggybacked output', async () => {
             this.modelUtils = await PaymentInFlightExitModelUtils.new(0, 0);
 
-            for (let i = 0; i++; i < MAX_OUTPUTS) {
+            for (let i = 0; i < MAX_OUTPUTS; i++) {
                 const actual = await this.modelUtils.isOutputPiggybacked(i);
                 expect(actual).to.be.false;
             }
@@ -72,7 +276,7 @@ contract('PaymentInFlightExitModelUtils', () => {
 
     describe('setInputPiggybacked', () => {
         const verifyPiggybacked = async () => {
-            for (let i = 0; i++; i < MAX_INPUTS) {
+            for (let i = 0; i < MAX_INPUTS; i++) {
                 await this.modelUtils.setInputPiggybacked(i);
                 const actual = await this.modelUtils.isInputPiggybacked(i);
                 expect(actual).to.be.true;
@@ -99,7 +303,7 @@ contract('PaymentInFlightExitModelUtils', () => {
 
     describe('setOutputPiggybacked', () => {
         const verifyPiggybacked = async () => {
-            for (let i = 0; i++; i < MAX_INPUTS) {
+            for (let i = 0; i < MAX_OUTPUTS; i++) {
                 await this.modelUtils.setOutputPiggybacked(i);
                 const actual = await this.modelUtils.isOutputPiggybacked(i);
                 expect(actual).to.be.true;
@@ -126,7 +330,7 @@ contract('PaymentInFlightExitModelUtils', () => {
 
     describe('clearInputPiggybacked', () => {
         const verifyPiggybacked = async () => {
-            for (let i = 0; i++; i < MAX_INPUTS) {
+            for (let i = 0; i < MAX_INPUTS; i++) {
                 await this.modelUtils.clearInputPiggybacked(i);
                 const actual = await this.modelUtils.isInputPiggybacked(i);
                 expect(actual).to.be.false;
@@ -153,7 +357,7 @@ contract('PaymentInFlightExitModelUtils', () => {
 
     describe('clearOutputPiggybacked', () => {
         const verifyPiggybacked = async () => {
-            for (let i = 0; i++; i < MAX_INPUTS) {
+            for (let i = 0; i < MAX_INPUTS; i++) {
                 await this.modelUtils.clearOutputPiggybacked(i);
                 const actual = await this.modelUtils.isOutputPiggybacked(i);
                 expect(actual).to.be.false;
