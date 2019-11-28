8a37326891203b1aef93db0454b105788cce36f1
Kevin Sullivan <4653170+kevsul@users.noreply.github.com>
2019-11-28 14:55:19 +0100

    test: more invalid rlp tests

--- plasma_framework/test/src/utils/RLP.test.js
+++ plasma_framework/test/src/utils/RLP.test.js
@@ -19,6 +19,12 @@ contract('RLP', () => {
         expect(actual.compare(expected)).to.equal(0);
     });
 
+    it('should fail decode to a bytes32 that has less than 32 bytes', async () => {
+        const expected = Buffer.alloc(31, 1);
+        const encoded = web3.utils.bytesToHex(rlp.encode(expected));
+        await expectRevert(this.test.decodeBytes32(encoded), 'Item length must be 33');
+    });
+
     it('should decode bytes20', async () => {
         const expected = Buffer.alloc(20, 1);
 
@@ -30,9 +36,19 @@ contract('RLP', () => {
     });
 
     it('should not decode an invalid length address', async () => {
-        const expected = '0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c00';
-        const encoded = web3.utils.bytesToHex(rlp.encode(expected));
+        const invalid21byteAddress = '0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c00';
+        const encoded = web3.utils.bytesToHex(rlp.encode(invalid21byteAddress));
         await expectRevert(this.test.decodeBytes20(encoded), 'Item length must be 21');
+    });
+
+    it('should not decode an invalid length address', async () => {
+        const invalidLengthInEncodedAddress = '0x950000000000000000000000000000000000000000';
+        await expectRevert(this.test.decodeBytes20(invalidLengthInEncodedAddress), 'Decoded item length must be 21');
+    });
+
+    it('should not decode an invalid length bytes32', async () => {
+        const invalidLengthInEncodedBytes32 = '0xa10000000000000000000000000000000000000000000000000000000000000000';
+        await expectRevert(this.test.decodeBytes32(invalidLengthInEncodedBytes32), 'Decoded item length must be 33');
     });
 
     it('should decode uint 0', async () => {
