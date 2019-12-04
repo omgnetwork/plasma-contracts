b2020b6cfa80a64e2e418766b374fd125beacf1f
boolafish <boolafish945@gmail.com>
2019-12-04 15:46:14 +0900

    feat: MoreVpFinalization library as simplification
    This commit adds a new lib MoreVpFinalization to handle the simplified version of
    tx finalization verification logic. The plan is to replace all exisiting usage of
    TxFinalizationVerifier and retire that afterward.
    
    related issue: https://github.com/omisego/plasma-contracts/issues/471

---
+++ plasma_framework/contracts/src/exits/utils/MoreVpFinalization.sol
@@ -0,0 +1,48 @@
+pragma solidity 0.5.11;
+pragma experimental ABIEncoderV2;
+
+import "../../framework/PlasmaFramework.sol";
+import "../../framework/Protocol.sol";
+import "../../utils/Merkle.sol";
+import "../../utils/TxPosLib.sol";
+
+/**
+ * @notice Library to check finalization for MoreVP protocol
+ */
+library MoreVpFinalization {
+    using TxPosLib for TxPosLib.TxPos;
+
+    /**
+    * @notice Checks whether a transaction is "standard finalized".
+    *         For MoreVP, it means the transaction should be included in a plasma block.
+    */
+    function isStandardFinalized(
+        PlasmaFramework framework,
+        bytes memory txBytes,
+        TxPosLib.TxPos memory txPos,
+        bytes memory inclusionProof
+    )
+        internal
+        view
+        returns (bool)
+    {
+        if (inclusionProof.length == 0) {
+            return false;
+        }
+
+        (bytes32 root,) = framework.blocks(txPos.blockNum());
+        require(root != bytes32(""), "Failed to get the root hash of the block num");
+
+        return Merkle.checkMembership(
+            txBytes, txPos.txIndex(), root, inclusionProof
+        );
+    }
+
+    /**
+    * @notice Checks whether a transaction is "protocol finalized"
+    *         For MoreVP, since it allows in-flight tx, so only checks for the existence of the transaction
+    */
+    function isProtocolFinalized(bytes memory txBytes) internal pure returns (bool) {
+        return txBytes.length > 0;
+    }
+}
