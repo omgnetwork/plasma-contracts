3e9e2a2522f297c0a04cfc71b6d3148d0a51eab7
Kevin Sullivan <4653170+kevsul@users.noreply.github.com>
2019-12-05 11:41:08 +0100

    fix: rlp checks for list when decoding non list items

--- plasma_framework/contracts/src/utils/RLPReader.sol
+++ plasma_framework/contracts/src/utils/RLPReader.sol
@@ -91,6 +91,7 @@ library RLPReader {
      */
     function toAddress(RLPItem memory item) internal pure returns (address) {
         require(item.len == 21, "Item length must be 21");
+        require(!isList(item), "Item must not be a list");
 
         (uint256 itemLen, uint256 offset) = decodeLengthAndOffset(item.memPtr);
         require(itemLen == 21, "Decoded item length must be 21");
@@ -114,6 +115,7 @@ library RLPReader {
      */
     function toUint(RLPItem memory item) internal pure returns (uint256) {
         require(item.len > 0 && item.len <= 33, "Item length must be between 1 and 33 bytes");
+        require(!isList(item), "Item must not be a list");
         (uint256 itemLen, uint256 offset) = decodeLengthAndOffset(item.memPtr);
         require(itemLen == item.len, "Decoded item length must be equal to the input data length");
 
@@ -148,6 +150,7 @@ library RLPReader {
     function toBytes32(RLPItem memory item) internal pure returns (bytes32) {
         // 1 byte for the length prefix
         require(item.len == 33, "Item length must be 33");
+        require(!isList(item), "Item must not be a list");
 
         (uint256 itemLen, uint256 offset) = decodeLengthAndOffset(item.memPtr);
         require(itemLen == 33, "Decoded item length must be 33");
