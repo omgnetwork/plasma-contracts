ad285fbacede758b3acdccfb2fb01ff6c3ab0d98
boolafish <boolafish945@gmail.com>
2019-12-05 16:42:09 +0900

    chore: add test coverage on protocol getter
    Coverage droppned while refactoring. As currently the value of 'MVP()' is not used.
    We only check protocol != More_VP() now. This commit add test to bring up the coverage.

--- plasma_framework/contracts/mocks/framework/ProtocolWrapper.sol
+++ plasma_framework/contracts/mocks/framework/ProtocolWrapper.sol
@@ -3,6 +3,16 @@ pragma solidity 0.5.11;
 import "../../src/framework/Protocol.sol";
 
 contract ProtocolWrapper {
+    // solhint-disable-next-line func-name-mixedcase
+    function MVP() public pure returns (uint8) {
+        return Protocol.MVP();
+    }
+
+    // solhint-disable-next-line func-name-mixedcase
+    function MORE_VP() public pure returns (uint8) {
+        return Protocol.MORE_VP();
+    }
+
     function isValidProtocol(uint8 protocol) public pure returns (bool) {
         return Protocol.isValidProtocol(protocol);
     }
--- plasma_framework/test/src/framework/Protocol.test.js
+++ plasma_framework/test/src/framework/Protocol.test.js
@@ -1,5 +1,6 @@
 const Protocol = artifacts.require('ProtocolWrapper');
 
+const { BN } = require('openzeppelin-test-helpers');
 const { expect } = require('chai');
 
 const { PROTOCOL } = require('../../helpers/constants.js');
@@ -7,6 +8,18 @@ const { PROTOCOL } = require('../../helpers/constants.js');
 contract('Protocol', () => {
     before(async () => {
         this.test = await Protocol.new();
+    });
+
+    describe('MORE_VP()', () => {
+        it('should return protocol value of MoreVP', async () => {
+            expect(await this.test.MORE_VP()).to.be.bignumber.equal(new BN(PROTOCOL.MORE_VP));
+        });
+    });
+
+    describe('MVP()', () => {
+        it('should return protocol value of MVP', async () => {
+            expect(await this.test.MVP()).to.be.bignumber.equal(new BN(PROTOCOL.MVP));
+        });
     });
 
     describe('isValidProtocol', () => {
