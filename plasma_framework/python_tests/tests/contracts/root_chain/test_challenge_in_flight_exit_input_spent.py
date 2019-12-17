f642120a4ddeb1096516ab78bdf99e991370e907
Piotr Żelazko <piotr.zelazko@imapp.pl>
2019-12-17 09:18:35 +0100

    chore: fix tests after rebase

--- plasma_framework/python_tests/tests/contracts/root_chain/test_challenge_in_flight_exit_input_spent.py
+++ plasma_framework/python_tests/tests/contracts/root_chain/test_challenge_in_flight_exit_input_spent.py
@@ -8,8 +8,8 @@ from plasma_core.constants import NULL_ADDRESS
 def test_challenge_in_flight_exit_input_spent_should_succeed(testlang, period):
     owner_1, owner_2, amount = testlang.accounts[0], testlang.accounts[1], 100
     deposit_id = testlang.deposit(owner_1, amount)
-    spend_id = testlang.spend_utxo([deposit_id], [owner_1])
-    double_spend_id = testlang.spend_utxo([deposit_id], [owner_1], [(owner_1.address, NULL_ADDRESS, 100)], force_invalid=True)
+    spend_id = testlang.spend_utxo([deposit_id], [owner_1], [(owner_1.address, NULL_ADDRESS, 100)])
+    double_spend_id = testlang.spend_utxo([deposit_id], [owner_1], [(owner_2.address, NULL_ADDRESS, 100)], force_invalid=True)
     testlang.start_in_flight_exit(spend_id)
     testlang.piggyback_in_flight_exit_input(spend_id, 0, owner_1)
     testlang.forward_to_period(period)
@@ -23,8 +23,8 @@ def test_challenge_in_flight_exit_input_spent_should_succeed(testlang, period):
 def test_challenge_in_flight_exit_input_spent_not_piggybacked_should_fail(testlang):
     owner_1, owner_2, amount = testlang.accounts[0], testlang.accounts[1], 100
     deposit_id = testlang.deposit(owner_1, amount)
-    spend_id = testlang.spend_utxo([deposit_id], [owner_1])
-    double_spend_id = testlang.spend_utxo([deposit_id], [owner_1], [(owner_1.address, NULL_ADDRESS, 100)], force_invalid=True)
+    spend_id = testlang.spend_utxo([deposit_id], [owner_1], [(owner_1.address, NULL_ADDRESS, 100)])
+    double_spend_id = testlang.spend_utxo([deposit_id], [owner_1], [(owner_2.address, NULL_ADDRESS, 100)], force_invalid=True)
     testlang.start_in_flight_exit(spend_id)
     testlang.forward_to_period(2)
 
@@ -35,7 +35,7 @@ def test_challenge_in_flight_exit_input_spent_not_piggybacked_should_fail(testla
 def test_challenge_in_flight_exit_input_spent_same_tx_should_fail(testlang):
     owner_1, owner_2, amount = testlang.accounts[0], testlang.accounts[1], 100
     deposit_id = testlang.deposit(owner_1, amount)
-    spend_id = testlang.spend_utxo([deposit_id], [owner_1])
+    spend_id = testlang.spend_utxo([deposit_id], [owner_1], [(owner_1.address, NULL_ADDRESS, 100)])
     testlang.start_in_flight_exit(spend_id)
     testlang.piggyback_in_flight_exit_input(spend_id, 0, owner_1)
     testlang.forward_to_period(2)
@@ -48,7 +48,7 @@ def test_challenge_in_flight_exit_input_spent_unrelated_tx_should_fail(testlang)
     owner_1, owner_2, amount = testlang.accounts[0], testlang.accounts[1], 100
     deposit_id_1 = testlang.deposit(owner_1, amount)
     deposit_id_2 = testlang.deposit(owner_1, amount)
-    spend_id = testlang.spend_utxo([deposit_id_1], [owner_1])
+    spend_id = testlang.spend_utxo([deposit_id_1], [owner_1], [(owner_1.address, NULL_ADDRESS, 100)])
     unrelated_spend_id = testlang.spend_utxo([deposit_id_2], [owner_1], [(owner_1.address, NULL_ADDRESS, 100)])
     testlang.start_in_flight_exit(spend_id)
     testlang.piggyback_in_flight_exit_input(spend_id, 0, owner_1)
@@ -61,8 +61,8 @@ def test_challenge_in_flight_exit_input_spent_unrelated_tx_should_fail(testlang)
 def test_challenge_in_flight_exit_input_spent_invalid_signature_should_fail(testlang):
     owner_1, owner_2, amount = testlang.accounts[0], testlang.accounts[1], 100
     deposit_id = testlang.deposit(owner_1, amount)
-    spend_id = testlang.spend_utxo([deposit_id], [owner_1])
-    double_spend_id = testlang.spend_utxo([deposit_id], [owner_2], [(owner_1.address, NULL_ADDRESS, 100)], force_invalid=True)
+    spend_id = testlang.spend_utxo([deposit_id], [owner_1], [(owner_1.address, NULL_ADDRESS, 100)])
+    double_spend_id = testlang.spend_utxo([deposit_id], [owner_2], [(owner_2.address, NULL_ADDRESS, 100)], force_invalid=True)
     testlang.start_in_flight_exit(spend_id)
     testlang.piggyback_in_flight_exit_input(spend_id, 0, owner_1)
     testlang.forward_to_period(2)
