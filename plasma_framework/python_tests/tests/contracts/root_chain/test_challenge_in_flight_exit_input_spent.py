5ee9e3936bb329347408b9db77273527a77c3bc3
boolafish <boolafish945@gmail.com>
2020-03-04 17:54:45 +0900

    test: integ test for all challenge indexes on IFE input spend
    Test in previous commit failed to cover the situation that would fail the contract with wrong index. This one
    test the index from another side (challenge input index) and it works to break the contract.
    
    Without the fix, it would result in "VM Exception while processing transaction: revert Output index out of bounds" error
    from the EVM.

--- plasma_framework/python_tests/tests/contracts/root_chain/test_challenge_in_flight_exit_input_spent.py
+++ plasma_framework/python_tests/tests/contracts/root_chain/test_challenge_in_flight_exit_input_spent.py
@@ -22,49 +22,94 @@ from plasma_core.utils.transactions import decode_utxo_id, encode_utxo_id
 
 
 
-@pytest.mark.parametrize("num_outputs", [1, 2, 3, 4])
-def test_challenge_in_flight_exit_should_succeed_for_all_output_indexes(testlang, num_outputs):
-    deposit_amount = 100
-    deposit_id = testlang.deposit(testlang.accounts[0], deposit_amount)
+# @pytest.mark.parametrize("num_outputs", [1, 2, 3, 4])
+# def test_challenge_in_flight_exit_should_succeed_for_all_output_indexes(testlang, num_outputs):
+#     deposit_amount = 100
+#     deposit_id = testlang.deposit(testlang.accounts[0], deposit_amount)
 
-    owners = []
-    outputs = []
-    tx_output_amount = deposit_amount // num_outputs
-    for i in range(0, num_outputs):
-        owners.append(testlang.accounts[i])
-        outputs.append((testlang.accounts[i].address, NULL_ADDRESS, tx_output_amount))
+#     owners = []
+#     outputs = []
+#     tx_output_amount = deposit_amount // num_outputs
+#     for i in range(0, num_outputs):
+#         owners.append(testlang.accounts[i])
+#         outputs.append((testlang.accounts[i].address, NULL_ADDRESS, tx_output_amount))
 
-    input_tx_id = testlang.spend_utxo([deposit_id], owners, outputs=outputs)
+#     input_tx_id = testlang.spend_utxo([deposit_id], owners, outputs=outputs)
 
-    blknum, tx_index, _ = decode_utxo_id(input_tx_id)
-    double_spend_output_index = num_outputs - 1
-    double_spend_owner = owners[double_spend_output_index]
-    double_spend_utxo = encode_utxo_id(blknum, tx_index, double_spend_output_index)
+#     blknum, tx_index, _ = decode_utxo_id(input_tx_id)
+#     double_spend_output_index = num_outputs - 1
+#     double_spend_owner = owners[double_spend_output_index]
+#     double_spend_utxo = encode_utxo_id(blknum, tx_index, double_spend_output_index)
 
-    ife_output_amount = tx_output_amount // 2
-    ife_tx_id = testlang.spend_utxo(
-        [double_spend_utxo],
-        [double_spend_owner],
-        [(owners[0].address, NULL_ADDRESS, ife_output_amount)]
+#     ife_output_amount = tx_output_amount // 2
+#     ife_tx_id = testlang.spend_utxo(
+#         [double_spend_utxo],
+#         [double_spend_owner],
+#         [(owners[0].address, NULL_ADDRESS, ife_output_amount)]
+#     )
+
+#     challenge_tx_id = testlang.spend_utxo(
+#         [double_spend_utxo],
+#         [double_spend_owner],
+#         [
+#             (owners[0].address, NULL_ADDRESS, ife_output_amount),
+#             (owners[0].address, NULL_ADDRESS, ife_output_amount)
+#         ],
+#         force_invalid=True
+#     )
+
+#     testlang.start_in_flight_exit(ife_tx_id)
+#     testlang.piggyback_in_flight_exit_input(ife_tx_id, 0, double_spend_owner)
+#     testlang.forward_to_period(1)
+#     testlang.challenge_in_flight_exit_input_spent(ife_tx_id, challenge_tx_id, double_spend_owner)
+
+#     in_flight_exit = testlang.get_in_flight_exit(ife_tx_id)
+#     assert not in_flight_exit.input_piggybacked(double_spend_output_index)
+
+
+@pytest.mark.parametrize("challenge_index", [0, 1, 2, 3])
+def test_challenge_in_flight_exit_should_succeed_for_all_challenge_indexes(testlang, challenge_index):
+    MAX_INDEX_SIZE = 4
+    alice, bob, carol = testlang.accounts[0], testlang.accounts[1], testlang.accounts[2]
+    amount = 100
+
+    deposit_id = testlang.deposit(alice, amount)
+
+    input_tx_id = testlang.spend_utxo(
+        [deposit_id],
+        [testlang.accounts[0]],
+        outputs=[(alice.address, NULL_ADDRESS, amount)]
     )
 
+    ife_tx_id = testlang.spend_utxo(
+        [input_tx_id],
+        [alice],
+        [(bob.address, NULL_ADDRESS, amount)]
+    )
+
+    inputs = []
+    for i in range(0, MAX_INDEX_SIZE):
+        if i == challenge_index:
+            inputs.append(input_tx_id)
+        else:
+            inputs.append(testlang.deposit(alice, amount))
+
     challenge_tx_id = testlang.spend_utxo(
-        [double_spend_utxo],
-        [double_spend_owner],
+        inputs,
+        [alice for i in range(0, MAX_INDEX_SIZE)],
         [
-            (owners[0].address, NULL_ADDRESS, ife_output_amount),
-            (owners[0].address, NULL_ADDRESS, ife_output_amount)
+            (carol.address, NULL_ADDRESS, amount),
         ],
         force_invalid=True
     )
 
     testlang.start_in_flight_exit(ife_tx_id)
-    testlang.piggyback_in_flight_exit_input(ife_tx_id, 0, double_spend_owner)
+    testlang.piggyback_in_flight_exit_input(ife_tx_id, 0, alice)
     testlang.forward_to_period(1)
-    testlang.challenge_in_flight_exit_input_spent(ife_tx_id, challenge_tx_id, double_spend_owner)
+    testlang.challenge_in_flight_exit_input_spent(ife_tx_id, challenge_tx_id, carol)
 
     in_flight_exit = testlang.get_in_flight_exit(ife_tx_id)
-    assert not in_flight_exit.input_piggybacked(double_spend_output_index)
+    assert not in_flight_exit.input_piggybacked(0)
 
 
 # def test_challenge_in_flight_exit_input_spent_not_piggybacked_should_fail(testlang):
