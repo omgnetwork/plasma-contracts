2f0218971153cb73bf640bcedcbf278dbbf9192a
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2020-03-04 13:42:36 +0100

    test: add a test checking all index-variants of ChallengeIFEOutputSpent

--- plasma_framework/python_tests/tests/contracts/root_chain/test_challenge_in_flight_exit_output_spent.py
+++ plasma_framework/python_tests/tests/contracts/root_chain/test_challenge_in_flight_exit_output_spent.py
@@ -2,6 +2,7 @@ import pytest
 from eth_utils import keccak
 from eth_tester.exceptions import TransactionFailed
 from plasma_core.constants import NULL_ADDRESS
+from plasma_core.utils.transactions import decode_utxo_id, encode_utxo_id
 
 
 # challenge should succeed even when phase 2 of in-flight exit is over
@@ -19,6 +20,57 @@ def test_challenge_in_flight_exit_output_spent_should_succeed(testlang, period):
 
     in_flight_exit = testlang.get_in_flight_exit(spend_id)
     assert not in_flight_exit.output_piggybacked(0)
+
+
+@pytest.mark.parametrize(
+    "ife_tx_num_outputs,double_spend_output_index,challenging_tx_num_inputs,double_spend_input_index",
+    [(i, j, k, l) for i in range(1, 5) for j in range(0, i) for k in range(1, 5) for l in range(0, k)]
+)
+def test_challenge_in_flight_exit_output_spent_should_succeed_for_all_indices(testlang,
+                                                                              ife_tx_num_outputs,
+                                                                              double_spend_output_index,
+                                                                              challenging_tx_num_inputs,
+                                                                              double_spend_input_index):
+    deposit_amount = 100
+    deposit_id = testlang.deposit(testlang.accounts[0], deposit_amount)
+
+    owners = []
+    outputs = []
+    tx_output_amount = deposit_amount // ife_tx_num_outputs
+    for i in range(0, ife_tx_num_outputs):
+        owners.append(testlang.accounts[i])
+        outputs.append((testlang.accounts[i].address, NULL_ADDRESS, tx_output_amount))
+
+    ife_tx_id = testlang.spend_utxo([deposit_id], owners, outputs=outputs)
+    blknum, tx_index, _ = decode_utxo_id(ife_tx_id)
+    double_spend_owner = owners[double_spend_output_index]
+    double_spend_utxo = encode_utxo_id(blknum, tx_index, double_spend_output_index)
+
+    testlang.start_in_flight_exit(ife_tx_id)
+    testlang.piggyback_in_flight_exit_output(ife_tx_id, double_spend_output_index, double_spend_owner)
+
+    MAX_INDEX_SIZE = 4
+    inputs = []
+    for i in range(0, MAX_INDEX_SIZE):
+        if i == double_spend_input_index:
+            inputs.append(double_spend_utxo)
+        else:
+            inputs.append(testlang.deposit(double_spend_owner, tx_output_amount))
+
+    challenge_tx_id = testlang.spend_utxo(
+        inputs,
+        [double_spend_owner for i in range(0, MAX_INDEX_SIZE)],
+        [
+            (double_spend_owner.address, NULL_ADDRESS, tx_output_amount),
+        ],
+        force_invalid=True
+    )
+
+    testlang.challenge_in_flight_exit_output_spent(ife_tx_id, challenge_tx_id, double_spend_output_index, double_spend_owner)
+
+    in_flight_exit = testlang.get_in_flight_exit(ife_tx_id)
+    for i in range(0, MAX_INDEX_SIZE):
+        assert not in_flight_exit.output_piggybacked(i)
 
 
 def test_challenge_in_flight_exit_output_spent_not_piggybacked_should_fail(testlang):
