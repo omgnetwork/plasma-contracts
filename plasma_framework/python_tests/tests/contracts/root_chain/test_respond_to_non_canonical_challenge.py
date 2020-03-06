de0f3ce0eab6872303b946acdd30b5362e9562a8
boolafish <boolafish945@gmail.com>
2020-03-06 16:57:05 +0900

    test: respond canonicity on all indices

--- plasma_framework/python_tests/tests/contracts/root_chain/test_respond_to_non_canonical_challenge.py
+++ plasma_framework/python_tests/tests/contracts/root_chain/test_respond_to_non_canonical_challenge.py
@@ -1,6 +1,8 @@
 import pytest
 from eth_tester.exceptions import TransactionFailed
 from plasma_core.constants import NULL_ADDRESS, MIN_EXIT_PERIOD
+from plasma_core.utils.transactions import decode_utxo_id, encode_utxo_id
+from tests_utils.constants import PAYMENT_TX_MAX_INPUT_SIZE, PAYMENT_TX_MAX_OUTPUT_SIZE
 
 
 # should succeed even when phase 2 of in-flight exit is over
@@ -20,6 +22,59 @@ def test_respond_to_non_canonical_challenge_should_succeed(testlang, period):
     in_flight_exit = testlang.get_in_flight_exit(spend_id)
     assert in_flight_exit.bond_owner == owner_1.address
     assert in_flight_exit.oldest_competitor == spend_id
+    assert in_flight_exit.is_canonical
+
+
+@pytest.mark.parametrize(
+    "double_spend_output_index,challenge_input_index",
+    [(i, j) for i in range(PAYMENT_TX_MAX_OUTPUT_SIZE) for j in range(PAYMENT_TX_MAX_INPUT_SIZE)]
+)
+def test_challenge_in_flight_exit_not_canonical_should_succeed_for_all_indices(testlang, double_spend_output_index, challenge_input_index):
+    alice, bob, carol = testlang.accounts[0], testlang.accounts[1], testlang.accounts[2]
+    deposit_amount = 100
+    deposit_id = testlang.deposit(alice, deposit_amount)
+
+    tx_output_amount = deposit_amount // PAYMENT_TX_MAX_OUTPUT_SIZE
+    outputs = [(alice.address, NULL_ADDRESS, tx_output_amount)] * PAYMENT_TX_MAX_OUTPUT_SIZE
+
+    input_tx_id = testlang.spend_utxo([deposit_id], [alice], outputs=outputs)
+    blknum, tx_index, _ = decode_utxo_id(input_tx_id)
+    double_spend_utxo = encode_utxo_id(blknum, tx_index, double_spend_output_index)
+
+    ife_output_amount = tx_output_amount
+    ife_tx_id = testlang.spend_utxo(
+        [double_spend_utxo],
+        [alice],
+        [(bob.address, NULL_ADDRESS, ife_output_amount)]
+    )
+
+    inputs = []
+    for i in range(0, PAYMENT_TX_MAX_INPUT_SIZE):
+        if i == challenge_input_index:
+            inputs.append(double_spend_utxo)
+        else:
+            inputs.append(testlang.deposit(alice, tx_output_amount))
+
+    challenge_tx_id = testlang.spend_utxo(
+        inputs,
+        [alice] * PAYMENT_TX_MAX_INPUT_SIZE,
+        [
+            (carol.address, NULL_ADDRESS, tx_output_amount),
+        ],
+        force_invalid=True
+    )
+
+    testlang.start_in_flight_exit(ife_tx_id)
+
+    testlang.challenge_in_flight_exit_not_canonical(ife_tx_id, challenge_tx_id, account=carol)
+
+    testlang.forward_to_period(1)
+
+    testlang.respond_to_non_canonical_challenge(ife_tx_id, alice)
+
+    in_flight_exit = testlang.get_in_flight_exit(ife_tx_id)
+    assert in_flight_exit.bond_owner == alice.address
+    assert in_flight_exit.oldest_competitor == ife_tx_id
     assert in_flight_exit.is_canonical
 
 
