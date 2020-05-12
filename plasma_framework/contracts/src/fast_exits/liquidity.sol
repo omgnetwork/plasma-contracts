3bca9f80bca382c065414315fa7951d6f1166fb8
souradeep-das <dsouradeep2@gmail.com>
2020-05-13 04:35:21 +0530

    added comments

--- plasma_framework/contracts/src/fast_exits/liquidity.sol
+++ plasma_framework/contracts/src/fast_exits/liquidity.sol
@@ -2,11 +2,11 @@ pragma solidity 0.5.11;
 pragma experimental ABIEncoderV2;
 
 import "../exits/payment/PaymentExitGame.sol";
-
+import "../framework/PlasmaFramework.sol";
 
 contract liquidity {
 
-   // some way to associate deposits to the contract with the sender
+   // some way to associate utxos to the contract with the sender, yes but how?
    // do we need a vault?
    // change to take in plasma framework contract address
     PaymentExitGame peg;
@@ -21,15 +21,18 @@ contract liquidity {
         return peg.startStandardExitBondSize();
     }
 
-   // check if the user calling has deposited and exiting his own utxo
+   // check if the user calling has transferred on omg network and exiting his own utxo
+   // but if this contract owns the utxo the transfer must have been done
+   // have to only check if the user is calling for his own utxo
     function startExitOnRootchainContract(uint256 _utxoPos, bytes memory _rlpOutputTx, bytes memory _outputTxInclusionProof) public payable {
         PaymentStandardExitRouterArgs.StartStandardExitArgs memory s;
         s.utxoPos = _utxoPos;
         s.rlpOutputTx = _rlpOutputTx;
         s.outputTxInclusionProof = _outputTxInclusionProof;
         peg.startStandardExit.value(msg.value)(s);
+        // store the resultant exitid as a trait for the nft and map it to the msg.sender
     }
 
     // logic to store ethers after getting the withdrawl back
-
+    // check if exitMap.exits[exitId] exists for the exitid assoicated with the token, if not burn the token and transfer
 }
