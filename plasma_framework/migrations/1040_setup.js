79b652366bb32642e7f442abb8be2b98e689ad10
Ino Murko <ino.murko@outlook.com>
2020-07-14 14:01:43 +0200

    split process into vault/ledger and dev

--- plasma_framework/migrations/1040_setup.js
+++ plasma_framework/migrations/1040_setup.js
@@ -11,6 +11,7 @@ const PaymentTransactionStateTransitionVerifier = artifacts.require('PaymentTran
 const childProcess = require('child_process');
 const config = require('../config.js');
 const pck = require('../package.json');
+// const LedgerWalletProvider = require('truffle-ledger-provider');
 
 module.exports = async (
     deployer,
@@ -18,39 +19,17 @@ module.exports = async (
     // eslint-disable-next-line no-unused-vars
     [deployerAddress, maintainerAddress, authorityAddress],
 ) => {
+    const vault = process.env.VAULT || false;
     const plasmaFramework = await PlasmaFramework.deployed();
     const sha = childProcess.execSync('git rev-parse HEAD').toString().trim().substring(0, 7);
-    await plasmaFramework.setVersion(`${pck.version}+${sha}`, { from: maintainerAddress });
-
-    // ETH vault registeration
-    console.log('Register eth vault');
     const ethDepositVerifier = await EthDepositVerifier.deployed();
     const ethVault = await EthVault.deployed();
-    await ethVault.setDepositVerifier(ethDepositVerifier.address, { from: maintainerAddress });
-    await plasmaFramework.registerVault(
-        config.registerKeys.vaultId.eth,
-        ethVault.address,
-        { from: maintainerAddress },
-    );
-
-    // ERC20 vault registeration
-    console.log('Register ERC20 vault');
     const erc20DepositVerifier = await Erc20DepositVerifier.deployed();
     const erc20Vault = await Erc20Vault.deployed();
-    await erc20Vault.setDepositVerifier(erc20DepositVerifier.address, { from: maintainerAddress });
-
-    await plasmaFramework.registerVault(
-        config.registerKeys.vaultId.erc20,
-        erc20Vault.address,
-        { from: maintainerAddress },
-    );
     const MORE_VP = config.frameworks.protocols.moreVp;
-    // 140 and 200, vaults need to be registered before we can deploy their exit games
     const PAYMENT_TX_TYPE = config.registerKeys.txTypes.payment;
-
     const spendingConditionRegistry = await SpendingConditionRegistry.deployed();
     const stateVerifier = await PaymentTransactionStateTransitionVerifier.deployed();
-
     const paymentExitGameArgs = [
         plasmaFramework.address,
         config.registerKeys.vaultId.eth,
@@ -60,37 +39,39 @@ module.exports = async (
         PAYMENT_TX_TYPE,
         config.frameworks.safeGasStipend.v1,
     ];
-    console.log('Deploy PaymentExitGame');
-    await deployer.deploy(PaymentExitGame, paymentExitGameArgs);
-    await deployer.deploy(FeeExitGame);
-
-
-    // Register PAYMENT EXIT GAME
-    console.log('Registering payment exit game');
     const paymentExitGame = await PaymentExitGame.deployed();
-    await plasmaFramework.registerExitGame(
-        PAYMENT_TX_TYPE,
-        paymentExitGame.address,
-        MORE_VP,
-        { from: maintainerAddress },
-    );
-    // Register FEE EXIT GAME
-    console.log('Registering fee exit game');
     const FEE_TX_TYPE = config.registerKeys.txTypes.fee;
     const feeExitGame = await FeeExitGame.deployed();
-    await plasmaFramework.registerExitGame(
-        FEE_TX_TYPE,
-        feeExitGame.address,
-        MORE_VP,
-        { from: maintainerAddress },
-    );
-
-    // activate the CHILDCHAIN and DONE
-    const vault = process.env.VAULT || false;
     if (vault) {
-        console.log('Hey Vaulty thingy.');
+        console.log('Yolo vault.');
     } else {
+        await ethVault.setDepositVerifier(ethDepositVerifier.address, { from: maintainerAddress });
+        await plasmaFramework.registerVault(
+            config.registerKeys.vaultId.eth,
+            ethVault.address,
+            { from: maintainerAddress },
+        );
+        await erc20Vault.setDepositVerifier(erc20DepositVerifier.address, { from: maintainerAddress });
+        await plasmaFramework.registerVault(
+            config.registerKeys.vaultId.erc20,
+            erc20Vault.address,
+            { from: maintainerAddress },
+        );
+        await deployer.deploy(PaymentExitGame, paymentExitGameArgs);
+        await deployer.deploy(FeeExitGame);
+        await plasmaFramework.registerExitGame(
+            PAYMENT_TX_TYPE,
+            paymentExitGame.address,
+            MORE_VP,
+            { from: maintainerAddress },
+        );
+        await plasmaFramework.registerExitGame(
+            FEE_TX_TYPE,
+            feeExitGame.address,
+            MORE_VP,
+            { from: maintainerAddress },
+        );
+        await plasmaFramework.setVersion(`${pck.version}+${sha}`, { from: maintainerAddress });
         await plasmaFramework.activateChildChain({ from: authorityAddress });
     }
-    
 };
