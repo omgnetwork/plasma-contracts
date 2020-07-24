4c5ee76a4ecc4687c2f21347b98c57cd0aea9279
Ino Murko <ino.murko@outlook.com>
2020-07-24 22:00:08 +0200

    set settings in vault, create wallet and account in vault

--- plasma_framework/migrations/1040_setup.js
+++ plasma_framework/migrations/1040_setup.js
@@ -13,7 +13,7 @@ const fs = require('fs');
 const path = require('path');
 const config = require('../config.js');
 const pck = require('../package.json');
-const util = require('util');
+// const util = require('util');
 
 module.exports = async (
     deployer,
@@ -43,9 +43,24 @@ module.exports = async (
     ];
     const FEE_TX_TYPE = config.registerKeys.txTypes.fee;
     if (vault) {
-        //web3.setProvider(new web3.providers.HttpProvider('http://localhost:7545'));
-        //web3.eth.net.isListening().then(console.log);
-        //console.log('me');
+//curl -X PUT -H "X-Vault-Token: $(vault print token)" -H "X-Vault-Request: true" -d '{"chain_id":"5777","rpc_url":"http://ganache:8545"}' http://127.0.0.1:8900/v1/immutability-eth-plugin/config
+//const walletName = 'plasma-deployer';
+//curl -X PUT -H "X-Vault-Request: true" -H "X-Vault-Token: $(vault print token)" -d 'null' http://127.0.0.1:8900/v1/immutability-eth-plugin/wallets/`${walletName}`
+//curl -X PUT -H "X-Vault-Request: true" -H "X-Vault-Token: $(vault print token)" -d 'null' http://127.0.0.1:8900/v1/immutability-eth-plugin/wallets/`${walletName}`/accounts
+// {
+//   "request_id": "4141b5c3-3ba7-beeb-64fe-cac2d75a1dc0",
+//   "lease_id": "",
+//   "lease_duration": 0,
+//   "renewable": false,
+//   "data": {
+//     "address": "0xA1296d36980058b1fe2Bb177b733FaC763d8405E",
+//     "blacklist": null,
+//     "index": 0,
+//     "whitelist": null
+//   },
+//   "warnings": null
+// }
+
         const buildDir = path.resolve(__dirname, '../../MultiSigWallet/build/multisig_instance');
         const gnosisMultisigAddress = fs.readFileSync(buildDir, 'utf8');
         const gnosisMultisigAbi = {
@@ -63,25 +78,46 @@ module.exports = async (
         };
         // ethVault.setDepositVerifier
         const setDepositVerifier = web3.eth.abi.encodeFunctionCall(ethVault.abi.find(o => o.name === 'setDepositVerifier'), [ethDepositVerifier.address]);
-        const gnosisSetDepositVerifier = web3.eth.abi.encodeFunctionCall(gnosisMultisigAbi, [ethVault.address, 0, setDepositVerifier])
-        await new Promise((resolve, reject) => web3.eth.sendTransaction({gas: 3000000, to: gnosisMultisigAddress, from: deployerAddress, data: gnosisSetDepositVerifier}, e => (e ? reject(e) : resolve())));
+        const gnosisSetDepositVerifier = web3.eth.abi.encodeFunctionCall(gnosisMultisigAbi, [ethVault.address, 0, setDepositVerifier]);
+        await new Promise((resolve, reject) => web3.eth.sendTransaction({ gas: 3000000, to: gnosisMultisigAddress, from: deployerAddress, data: gnosisSetDepositVerifier }, e => (e ? reject(e) : resolve())));
         // plasmaFramework.registerVault
         const registerVault = web3.eth.abi.encodeFunctionCall(plasmaFramework.abi.find(o => o.name === 'registerVault'), [config.registerKeys.vaultId.eth, ethVault.address]);
-        const gnosisRegisterVault = web3.eth.abi.encodeFunctionCall(gnosisMultisigAbi, [plasmaFramework.address, 0, registerVault])
-        await new Promise((resolve, reject) => web3.eth.sendTransaction({gas: 3000000, to: gnosisMultisigAddress, from: deployerAddress, data: gnosisRegisterVault}, e => (e ? reject(e) : resolve())));
+        const gnosisRegisterVault = web3.eth.abi.encodeFunctionCall(gnosisMultisigAbi, [plasmaFramework.address, 0, registerVault]);
+        await new Promise((resolve, reject) => web3.eth.sendTransaction({ gas: 3000000, to: gnosisMultisigAddress, from: deployerAddress, data: gnosisRegisterVault }, e => (e ? reject(e) : resolve())));
 
         // ERC20 ethVault.setDepositVerifier
         const setERC20DepositVerifier = web3.eth.abi.encodeFunctionCall(erc20Vault.abi.find(o => o.name === 'setDepositVerifier'), [erc20DepositVerifier.address]);
-        const gnosisERC20SetDepositVerifier = web3.eth.abi.encodeFunctionCall(gnosisMultisigAbi, [erc20Vault.address, 0, setERC20DepositVerifier])
-        await new Promise((resolve, reject) => web3.eth.sendTransaction({gas: 3000000, to: gnosisMultisigAddress, from: deployerAddress, data: gnosisERC20SetDepositVerifier}, e => (e ? reject(e) : resolve())));
+        const gnosisERC20SetDepositVerifier = web3.eth.abi.encodeFunctionCall(gnosisMultisigAbi, [erc20Vault.address, 0, setERC20DepositVerifier]);
+        await new Promise((resolve, reject) => web3.eth.sendTransaction({ gas: 3000000, to: gnosisMultisigAddress, from: deployerAddress, data: gnosisERC20SetDepositVerifier }, e => (e ? reject(e) : resolve())));
         // plasmaFramework.registerVault
         const registerERC20Vault = web3.eth.abi.encodeFunctionCall(plasmaFramework.abi.find(o => o.name === 'registerVault'), [config.registerKeys.vaultId.erc20, erc20Vault.address]);
-        const gnosisERC20RegisterVault = web3.eth.abi.encodeFunctionCall(gnosisMultisigAbi, [plasmaFramework.address, 0, registerERC20Vault])
-        await new Promise((resolve, reject) => web3.eth.sendTransaction({gas: 3000000, to: gnosisMultisigAddress, from: deployerAddress, data: gnosisERC20RegisterVault}, e => (e ? reject(e) : resolve())));
-        await deployer.deploy(FeeExitGame);
-        // the below deployment fails because vaults are not registed yet!
+        const gnosisERC20RegisterVault = web3.eth.abi.encodeFunctionCall(gnosisMultisigAbi, [plasmaFramework.address, 0, registerERC20Vault]);
+        await new Promise((resolve, reject) => web3.eth.sendTransaction({ gas: 3000000, to: gnosisMultisigAddress, from: deployerAddress, data: gnosisERC20RegisterVault }, e => (e ? reject(e) : resolve())));
+        // init payment exit game!
+        // await deployer.deploy(FeeExitGame);
+        // the below deployment fails because vaults are not registered yet!
         // https://github.com/omgnetwork/plasma-contracts/issues/656
-        await deployer.deploy(PaymentExitGame, paymentExitGameArgs);
+        //await deployer.deploy(PaymentExitGame, paymentExitGameArgs);
+
+        // plasmaFramework.registerExitGame PAYMENT_TX_TYPE
+        const registerExitGame = web3.eth.abi.encodeFunctionCall(plasmaFramework.abi.find(o => o.name === 'registerExitGame'), [PAYMENT_TX_TYPE, paymentExitGame.address, MORE_VP]);
+        const gnosisRegisterExitGame = web3.eth.abi.encodeFunctionCall(gnosisMultisigAbi, [plasmaFramework.address, 0, registerExitGame]);
+        await new Promise((resolve, reject) => web3.eth.sendTransaction({ gas: 3000000, to: gnosisMultisigAddress, from: deployerAddress, data: gnosisRegisterExitGame }, e => (e ? reject(e) : resolve())));
+        
+        // plasmaFramework.registerExitGame FEE_TX_TYPE
+        const registerFeeExitGame = web3.eth.abi.encodeFunctionCall(plasmaFramework.abi.find(o => o.name === 'registerExitGame'), [FEE_TX_TYPE, feeExitGame.address, MORE_VP]);
+        const gnosisFeeRegisterExitGame = web3.eth.abi.encodeFunctionCall(gnosisMultisigAbi, [plasmaFramework.address, 0, registerFeeExitGame]);
+        await new Promise((resolve, reject) => web3.eth.sendTransaction({ gas: 3000000, to: gnosisMultisigAddress, from: deployerAddress, data: gnosisFeeRegisterExitGame }, e => (e ? reject(e) : resolve())));
+
+        // set version
+        const setVersion = web3.eth.abi.encodeFunctionCall(plasmaFramework.abi.find(o => o.name === 'setVersion'), [`${pck.version}+${sha}`]);
+        const gnosisSetVersion = web3.eth.abi.encodeFunctionCall(gnosisMultisigAbi, [plasmaFramework.address, 0, setVersion]);
+        await new Promise((resolve, reject) => web3.eth.sendTransaction({ gas: 3000000, to: gnosisMultisigAddress, from: deployerAddress, data: gnosisSetVersion }, e => (e ? reject(e) : resolve())));
+
+        // activate child chain via Vault
+//
+//curl -X PUT -H "X-Vault-Request: true" -H "X-Vault-Token: $(vault print token)" -d '{"contract":"0xd185aff7fb18d2045ba766287ca64992fdd79b1e"}' http://127.0.0.1:8900/v1/immutability-eth-plugin/wallets/plasma-deployer/accounts/0x888a65279D4a3A4E3cbA57D5B3Bd3eB0726655a6/plasma/activateChildChain
+//
         
     } else {
         await ethVault.setDepositVerifier(ethDepositVerifier.address, { from: maintainerAddress });
--- plasma_framework/migrations/10_initial_checks_and_migrations.js
+++ plasma_framework/migrations/10_initial_checks_and_migrations.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 /* eslint-disable prefer-destructuring */
-
+const https = require('https');
 const Migrations = artifacts.require('Migrations');
 
 const fundAddressIfEmpty = async (from, to, value, receiverName) => {
@@ -33,19 +33,96 @@ module.exports = async (
     // eslint-disable-next-line no-unused-vars
     [deployerAddress, maintainerAddress, authorityAddress],
 ) => {
-    console.log(`Deployer address: ${deployerAddress}`);
-    console.log(`Maintainer address: ${maintainerAddress}`);
-    console.log(`Authority address: ${authorityAddress}`);
+    var authority = authorityAddress;
+    const vault = process.env.VAULT || false;
+    if (vault) {
+        const chain_id = `${process.env.CHAIN_ID}` || 1;
+        const rpc_url = process.env.REMOTE_URL || 'http://127.0.0.1:8545';
+        const walletName = 'plasma-deployer';
+        // configuration
+        var options = {
+            host: `${process.env.VAULT_ADDR}`,
+            port: `${process.env.VAULT_PORT}`,
+            path: '/v1/immutability-eth-plugin/config',
+            method: 'PUT',
+            headers: { 
+                "X-Vault-Token" : `${process.env.VAULT_TOKEN}`,
+                "X-Vault-Request" : true,
+            }
+        };
+        var callback = function(response) {
+            var str = ''
+            response.on('data', function(chunk){
+                str += chunk
+            })
 
+            response.on('end', function(){
+                const response_object = JSON.parse(str);
+                if (response_object.data.chain_id != chain_id || response_object.data.rpc_url != rpc_url){
+                    throw 'Vault configuration did not stick';
+                }
+            })
+        }
+        var body = JSON.stringify({
+            chain_id: chain_id,
+            rpc_url: rpc_url
+        });
+        https.request(options, callback).end(body);
+        // wallet 
+        options.path = `/v1/immutability-eth-plugin/wallets/${walletName}`;
+
+        callback = function(response) {
+
+            var str = ''
+            response.on('data', function(chunk){
+                str += chunk
+            })
+
+            response.on('end', function(){
+                const response_object = JSON.parse(str);
+                if (typeof response_object.request_id !== 'string'){
+                    throw 'Creating wallet failed';
+                }
+            })
+        }
+        https.request(options, callback).end(JSON.stringify({}));
+        // account
+        options.path = `/v1/immutability-eth-plugin/wallets/${walletName}/accounts`;
+
+        callback = function(response) {
+
+            var str = ''
+            response.on('data', function(chunk){
+                str += chunk
+            })
+
+            response.on('end', function(){
+                const response_object = JSON.parse(str);
+                console.log(response_object);
+                if (typeof response_object.data.address !== 'string'){
+                    throw 'Creating account failed';
+                }else{
+                    authority = response_object.data.address;
+                }
+            })
+        }
+        await https.request(options, callback).end(JSON.stringify({}));
+
+    }else{
+        console.log(`Deployer address: ${deployerAddress}`);
+        console.log(`Maintainer address: ${maintainerAddress}`);
+        
+    }
+    console.log(`Authority address: ${authority}`);
     const initAmountForMaintainer = process.env.MAINTAINER_ADDRESS_INITIAL_AMOUNT || 2e17; // 0.2 ETH by default
     const initAmountForAuthority = process.env.AUTHORITY_ADDRESS_INITIAL_AMOUNT || 2e17; // 0.2 ETH by default
 
     await fundAddressIfEmpty(deployerAddress, maintainerAddress, initAmountForMaintainer, 'maintainer');
-    await fundAddressIfEmpty(deployerAddress, authorityAddress, initAmountForAuthority, 'authority');
+    await fundAddressIfEmpty(deployerAddress, authority, initAmountForAuthority, 'authority');
 
     await outputAddressFunds(deployerAddress, 'Deployer');
     await outputAddressFunds(maintainerAddress, 'Maintainer');
-    await outputAddressFunds(authorityAddress, 'Authority');
+    await outputAddressFunds(authority, 'Authority');
 
     console.log('\n########################### Notice ############################');
     console.log('It is recommended to have 0.2 ETH in the maintainer and authority address');
