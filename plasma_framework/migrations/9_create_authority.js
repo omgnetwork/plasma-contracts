fec3c2063d8a2f1d3960292d89b00e4f2f6fb1c0
ino.murko@outlook.com <ino.murko@outlook.com>
2020-08-04 16:40:58 +0200

    use lib for https

--- plasma_framework/migrations/9_create_authority.js
+++ plasma_framework/migrations/9_create_authority.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 /* eslint-disable prefer-destructuring */
-const https = require('https');
+const axios = require('axios');
 const fs = require('fs');
 
 module.exports = async (
@@ -17,74 +17,46 @@ module.exports = async (
         const walletName = 'plasma-deployer';
         // configuration
         const options = {
-            host: `${process.env.VAULT_ADDR}`,
-            port: `${process.env.VAULT_PORT}`,
-            path: '/v1/immutability-eth-plugin/config',
+            url: `${process.env.VAULT_ADDR}/v1/immutability-eth-plugin/config`,
             method: 'PUT',
             headers: {
                 'X-Vault-Token': `${process.env.VAULT_TOKEN}`,
                 'X-Vault-Request': true,
             },
+            data: JSON.stringify({
+                chain_id: chainId,
+                rpc_url: rpcUrl,
+            }),
         };
         console.log('Configuring vault');
-        let callback = resolve => function (response) {
-            let str = '';
-            response.on('data', function (chunk) {
-                str += chunk;
-            });
-
-            response.on('end', function () {
-                const responseObject = JSON.parse(str);
-                if (responseObject.data.chain_id !== chainId && responseObject.data.rpc_url !== rpcUrl) {
-                    throw new Error('Vault configuration did not stick');
-                }
-                resolve();
-            });
-        };
-        const body = JSON.stringify({
-            chain_id: chainId,
-            rpc_url: rpcUrl,
+        await axios(options).then((response) => {
+            const responseObject = response.data;
+            if (responseObject.data.chain_id !== chainId && responseObject.data.rpc_url !== rpcUrl) {
+                throw new Error('Vault configuration did not stick');
+            }
         });
-        await new Promise(resolve => https.request(options, callback(resolve)).end(body));
         // wallet
         console.log('Creating wallet');
-        options.path = `/v1/immutability-eth-plugin/wallets/${walletName}`;
-
-        callback = resolve => function (response) {
-            let str = '';
-            response.on('data', function (chunk) {
-                str += chunk;
-            });
-            response.on('end', function () {
-                const responseObject = JSON.parse(str);
-                if (typeof responseObject.request_id !== 'string') {
-                    throw new Error('Creating wallet failed');
-                }
-                resolve();
-            });
-        };
-        await new Promise(resolve => https.request(options, callback(resolve)).end(JSON.stringify({})));
+        options.url = `${process.env.VAULT_ADDR}/v1/immutability-eth-plugin/wallets/${walletName}`;
+        delete options.data;
+        await axios(options).then((response) => {
+            const responseObject = response.data;
+            if (typeof responseObject.request_id !== 'string') {
+                throw new Error('Creating wallet failed');
+            }
+        });
         // account
         console.log('Creating account');
-        options.path = `/v1/immutability-eth-plugin/wallets/${walletName}/accounts`;
-
-        callback = resolve => function (response) {
-            let str = '';
-            response.on('data', function (chunk) {
-                str += chunk;
-            });
-            response.on('end', function () {
-                const responseObject = JSON.parse(str);
-                if (typeof responseObject.data.address !== 'string') {
-                    throw new Error('Creating account failed');
-                } else {
-                    console.log(`Authority account now in vault ${responseObject.data.address}`);
-                    fs.writeFileSync('vault_authority', `${responseObject.data.address}`.toLowerCase());
-                }
-                resolve();
-            });
-        };
-        await new Promise(resolve => https.request(options, callback(resolve)).end(JSON.stringify({})));
+        options.url = `${process.env.VAULT_ADDR}/v1/immutability-eth-plugin/wallets/${walletName}/accounts`;
+        await axios(options).then((response) => {
+            const responseObject = response.data;
+            if (typeof responseObject.data.address !== 'string') {
+                throw new Error('Creating account failed');
+            } else {
+                console.log(`Authority account now in vault ${responseObject.data.address}`);
+                fs.writeFileSync('vault_authority', `${responseObject.data.address}`.toLowerCase());
+            }
+        });
         console.log('Done');
     } else {
         console.log(`Skipping because Vault ${vault} or perhaps authority exists ${authorityExists}`);
