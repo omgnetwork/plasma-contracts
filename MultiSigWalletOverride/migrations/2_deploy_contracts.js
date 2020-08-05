86a30f1c8f30ade294b24f775be92079bf9b3c16
Ino Murko <ino.murko@outlook.com>
2020-08-05 17:09:28 +0200

    gnosis submodule

--- MultiSigWalletOverride/migrations/2_deploy_contracts.js
+++ MultiSigWalletOverride/migrations/2_deploy_contracts.js
@@ -5,24 +5,23 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = deployer => {
-  const args = process.argv.slice()
+  const args = process.argv.slice();
   if (process.env.DEPLOY_FACTORY){
-    deployer.deploy(MultisigWalletFactory)
-    console.log("Factory with Daily Limit deployed")
+    deployer.deploy(MultisigWalletFactory);
+    console.log("Factory with Daily Limit deployed");
   } else if (args.length < 5) {
     console.error("Multisig with daily limit requires to pass owner " +
-      "list, required confirmations and daily limit")
+      "list, required confirmations and daily limit");
   } else if (args.length < 6) {
-    deployer.deploy(MultisigWalletWithoutDailyLimit, args[3].split(","), args[4])
-    const multisigWalletWithoutDailyLimit = await MultisigWalletWithoutDailyLimit.deployed();
+    console.log(deployer.deploy(MultisigWalletWithoutDailyLimit, args[3].split(","), args[4]));
     const buildDir = path.resolve(__dirname, '../build');
     if (!fs.existsSync(buildDir)) {
         fs.mkdirSync(buildDir);
     }
-    fs.writeFileSync(path.resolve(buildDir, 'multisig_instance'), `${multisigWalletWithoutDailyLimit.address}`.toLowerCase());
-    console.log("Wallet deployed")
+    fs.writeFileSync(path.resolve(buildDir, 'multisig_instance'), `${MultisigWalletWithoutDailyLimit.address}`.toLowerCase());
+    console.log("Wallet deployed");
   } else {
-    deployer.deploy(MultisigWalletWithDailyLimit, args[3].split(","), args[4], args[5])
-    console.log("Wallet with Daily Limit deployed")
+    deployer.deploy(MultisigWalletWithDailyLimit, args[3].split(","), args[4], args[5]);
+    console.log("Wallet with Daily Limit deployed");
   }
 }
