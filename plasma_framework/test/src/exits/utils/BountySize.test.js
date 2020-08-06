f0e70770026955adb10ebda636ca74729f4a4de4
souradeep-das <dsouradeep2@gmail.com>
2020-08-07 01:59:29 +0530

    style: js lint

--- plasma_framework/test/src/exits/utils/BountySize.test.js
+++ plasma_framework/test/src/exits/utils/BountySize.test.js
@@ -12,15 +12,14 @@ contract('ExitBounty', () => {
     describe('calculate process standard exit bounty', () => {
         it('should return the correct bounty for a gas price of 10 gwei', async () => {
             const gasPrice = 10000000000;
-            expect(await this.contract.processStandardExitBountySize({ gasPrice: gasPrice }))
+            expect(await this.contract.processStandardExitBountySize({ gasPrice }))
                 .to.be.bignumber.equal(new BN(GAS_USED_PROCESS_EXIT * gasPrice));
         });
 
         it('should return the correct bounty for a gas price of 80 gwei', async () => {
             const gasPrice = 80000000000;
-            expect(await this.contract.processStandardExitBountySize({ gasPrice: gasPrice }))
+            expect(await this.contract.processStandardExitBountySize({ gasPrice }))
                 .to.be.bignumber.equal(new BN(GAS_USED_PROCESS_EXIT * gasPrice));
         });
-
     });
 });
