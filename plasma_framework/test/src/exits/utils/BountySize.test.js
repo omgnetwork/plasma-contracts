e4cb0783e9689fe8569199dbd00665291f040d9a
souradeep-das <dsouradeep2@gmail.com>
2020-08-17 19:35:22 +0530

    feat: add lib tests

--- plasma_framework/test/src/exits/utils/BountySize.test.js
+++ plasma_framework/test/src/exits/utils/BountySize.test.js
@@ -4,6 +4,7 @@ const { expect } = require('chai');
 
 contract('ExitBounty', () => {
     const GAS_USED_PROCESS_EXIT = 107000; // approx gas usage of processExit
+    const GAS_SPLIT_PER_PIGGYBACK = 500000; // approx gas usage split per piggyback
 
     before('setup', async () => {
         this.contract = await ExitBounty.new();
@@ -11,15 +12,33 @@ contract('ExitBounty', () => {
 
     describe('calculate process standard exit bounty', () => {
         it('should return the correct bounty for a gas price of 10 gwei', async () => {
-            const gasPrice = 10000000000;
+            const gasPrice = new BN('10000000000');
+            const expectedSize = gasPrice.mul(new BN(GAS_USED_PROCESS_EXIT));
             expect(await this.contract.processStandardExitBountySize(gasPrice))
-                .to.be.bignumber.equal(new BN(GAS_USED_PROCESS_EXIT * gasPrice));
+                .to.be.bignumber.equal(expectedSize);
         });
 
         it('should return the correct bounty for a gas price of 80 gwei', async () => {
-            const gasPrice = 80000000000;
+            const gasPrice = new BN('80000000000');
+            const expectedSize = gasPrice.mul(new BN(GAS_USED_PROCESS_EXIT));
             expect(await this.contract.processStandardExitBountySize(gasPrice))
-                .to.be.bignumber.equal(new BN(GAS_USED_PROCESS_EXIT * gasPrice));
+                .to.be.bignumber.equal(expectedSize);
+        });
+    });
+
+    describe('calculate process in-flight exit bounty', () => {
+        it('should return the correct bounty for a gas price of 10 gwei', async () => {
+            const gasPrice = new BN('10000000000');
+            const expectedSize = gasPrice.mul(new BN(GAS_SPLIT_PER_PIGGYBACK));
+            expect(await this.contract.processInFlightExitBountySize(gasPrice))
+                .to.be.bignumber.equal(expectedSize);
+        });
+
+        it('should return the correct bounty for a gas price of 80 gwei', async () => {
+            const gasPrice = new BN('80000000000');
+            const expectedSize = gasPrice.mul(new BN(GAS_SPLIT_PER_PIGGYBACK));
+            expect(await this.contract.processInFlightExitBountySize(gasPrice))
+                .to.be.bignumber.equal(expectedSize);
         });
     });
 });
