70e00750ed8972bbf05ac2cd17f54958a8d4191a
Ino Murko <ino.murko@outlook.com>
2020-08-19 12:27:30 +0200

    Inomurko/vault+authority (#678)
    * migrate with vault and gnosis

--- plasma_framework/migrations/140_payment_exit_game.js
+++ plasma_framework/migrations/140_payment_exit_game.js
@@ -4,7 +4,6 @@ const PaymentExitGame = artifacts.require('PaymentExitGame');
 const PaymentTransactionStateTransitionVerifier = artifacts.require('PaymentTransactionStateTransitionVerifier');
 const PlasmaFramework = artifacts.require('PlasmaFramework');
 const SpendingConditionRegistry = artifacts.require('SpendingConditionRegistry');
-
 const config = require('../config.js');
 
 module.exports = async (
@@ -14,11 +13,9 @@ module.exports = async (
     [deployerAddress, maintainerAddress, authorityAddress],
 ) => {
     const PAYMENT_TX_TYPE = config.registerKeys.txTypes.payment;
-
     const spendingConditionRegistry = await SpendingConditionRegistry.deployed();
     const stateVerifier = await PaymentTransactionStateTransitionVerifier.deployed();
     const plasmaFramework = await PlasmaFramework.deployed();
-
     const paymentExitGameArgs = [
         plasmaFramework.address,
         config.registerKeys.vaultId.eth,
@@ -29,13 +26,4 @@ module.exports = async (
         config.frameworks.safeGasStipend.v1,
     ];
     await deployer.deploy(PaymentExitGame, paymentExitGameArgs);
-    const paymentExitGame = await PaymentExitGame.deployed();
-    await paymentExitGame.init({ from: maintainerAddress });
-    // register the exit game to framework
-    await plasmaFramework.registerExitGame(
-        PAYMENT_TX_TYPE,
-        paymentExitGame.address,
-        config.frameworks.protocols.moreVp,
-        { from: maintainerAddress },
-    );
 };
--- plasma_framework/test/loadTests/PriorityQueue.load.test.js
+++ plasma_framework/test/loadTests/PriorityQueue.load.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const PriorityQueue = artifacts.require('PriorityQueue');
 const LoadTest = artifacts.require('PriorityQueueLoadTest');
 
