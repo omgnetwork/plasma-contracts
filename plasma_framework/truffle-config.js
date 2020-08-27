851231bb2ac432ed933a6f73e3b9f690bed53f07
souradeep-das <dsouradeep2@gmail.com>
2020-08-27 22:21:56 +0530

    check deployment

--- plasma_framework/truffle-config.js
+++ plasma_framework/truffle-config.js
@@ -1,6 +1,7 @@
 require('dotenv').config(); // auto parse env variables from '.env' file
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
+const NonceTrackerSubprovider = require("web3-provider-engine/subproviders/nonce-tracker")
 
 module.exports = {
     networks: {
@@ -21,7 +22,8 @@ module.exports = {
         // before getting submitted to the remote client.
         remote: {
             skipDryRun: true,
-            provider: () => new HDWalletProvider(
+            provider: function () {
+                var wallet = new HDWalletProvider(
                 [
                     process.env.DEPLOYER_PRIVATEKEY || '0'.repeat(64),
                     process.env.MAINTAINER_PRIVATEKEY || '0'.repeat(64),
@@ -30,6 +32,11 @@ module.exports = {
                 process.env.REMOTE_URL || 'http://127.0.0.1:8545',
                 0, 3,
             ),
+                var nonceTracker = new NonceTrackerSubprovider()
+                wallet.engine._providers.unshift(nonceTracker)
+                nonceTracker.setEngine(wallet.engine)
+                return wallet
+            },
             gasPrice: process.env.GAS_PRICE || 20000000000, // default 20 gwei
             network_id: '*',
         },
