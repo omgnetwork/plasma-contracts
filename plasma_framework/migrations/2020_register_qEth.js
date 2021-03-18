279b813f5ffb549b20164644682989332c76a0d1
souradeep-das <dsouradeep2@gmail.com>
2021-03-18 21:35:32 +0530

    add deploy flag

--- plasma_framework/migrations/2020_register_qEth.js
+++ plasma_framework/migrations/2020_register_qEth.js
@@ -7,11 +7,14 @@ module.exports = async (
     // eslint-disable-next-line no-unused-vars
     [deployerAddress, maintainerAddress, authorityAddress],
 ) => {
-    const quasar = await Quasar.deployed();
-    const qEth = await deployer.deploy(QToken, 'Quasar Ether', 'qETH', 18, quasar.address);
-    const quasarFee = process.env.QETH_FEE;
+    const deployQuasar = process.env.DEPLOY_QUASAR || false;
+    if (deployQuasar) {
+        const quasar = await Quasar.deployed();
+        const qEth = await deployer.deploy(QToken, 'Quasar Ether', 'qETH', 18, quasar.address);
+        const quasarFee = process.env.QETH_FEE;
 
-    await quasar.registerQToken(
-        '0x0000000000000000000000000000000000000000', qEth.address, quasarFee,
-    );
+        await quasar.registerQToken(
+            '0x0000000000000000000000000000000000000000', qEth.address, quasarFee,
+        );
+    }
 };
