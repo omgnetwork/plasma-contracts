6eba60181cd20d6016470e3c9309d0d2cddd8f46
souradeep-das <dsouradeep2@gmail.com>
2021-03-18 21:31:30 +0530

    feat: add quasar migration scripts

---
+++ plasma_framework/migrations/2020_register_qEth.js
@@ -0,0 +1,17 @@
+const Quasar = artifacts.require('Quasar');
+const QToken = artifacts.require('QToken');
+
+module.exports = async (
+    deployer,
+    _,
+    // eslint-disable-next-line no-unused-vars
+    [deployerAddress, maintainerAddress, authorityAddress],
+) => {
+    const quasar = await Quasar.deployed();
+    const qEth = await deployer.deploy(QToken, 'Quasar Ether', 'qETH', 18, quasar.address);
+    const quasarFee = process.env.QETH_FEE;
+
+    await quasar.registerQToken(
+        '0x0000000000000000000000000000000000000000', qEth.address, quasarFee,
+    );
+};
